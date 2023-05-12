2fae724d283ee16a7a889bcad5800a1ee3240ccf
Timm Ortloff <timm.ortloff@icinga.com>
2023-05-12 15:24:36 +0200

    Add detail view items

--- library/Kubernetes/Web/DeploymentDetail.php
+++ library/Kubernetes/Web/DeploymentDetail.php
@@ -6,6 +6,8 @@ use Icinga\Module\Kubernetes\Model\Deployment;
 use ipl\Html\Attributes;
 use ipl\Html\BaseHtmlElement;
 use ipl\Html\HtmlElement;
+use ipl\Html\Text;
+use ipl\Web\Widget\TimeAgo;
 
 class DeploymentDetail extends BaseHtmlElement
 {
@@ -21,9 +23,23 @@ class DeploymentDetail extends BaseHtmlElement
 
     protected function assemble()
     {
-        foreach ($this->item->getColumns() as $col) {
-            $this->add(new HtmlElement('li', new Attributes(['value' => $col, 'class' => 'deployment-detail-item'])));
-        }
+        $this->add(new HtmlElement('li', new Attributes(['class' => 'deployment-detail-item']),
+            new Text(sprintf('UID: %s', $this->item->uid))));
+
+        $this->add(new HtmlElement('li', new Attributes(['class' => 'deployment-detail-item']),
+            new Text(sprintf('Namespace/Name: %s/%s', $this->item->namespace, $this->item->name))));
+
+        $this->add(new HtmlElement('li', new Attributes(['class' => 'deployment-detail-item']),
+            new Text(sprintf('Replicas: %d/%d', $this->item->ready_replicas, $this->item->replicas))));
+
+        $this->add(new HtmlElement('li', new Attributes(['class' => 'deployment-detail-item']),
+            new Text(sprintf('Unavailable Replicas: %d', $this->item->unavailable_replicas))));
+
+        $this->add(new HtmlElement('li', new Attributes(['class' => 'deployment-detail-item']),
+            new Text(sprintf('Strategy: %s', $this->item->strategy))));
+
+        $this->add(new HtmlElement('li', new Attributes(['class' => 'deployment-detail-item']),
+            new Text('Created: '), new TimeAgo($this->item->created->getTimestamp())));
     }
 
 }
