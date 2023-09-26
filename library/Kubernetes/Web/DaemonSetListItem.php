1c1885e89bfec5b90e0c0a2a6dfcaaa95f2bfaa7
Florian Strohmaier <florian.strohmaier@icinga.com>
2023-09-26 16:54:08 +0200

    DaemonSetListItem: Update design

--- library/Kubernetes/Web/DaemonSetListItem.php
+++ library/Kubernetes/Web/DaemonSetListItem.php
@@ -2,24 +2,31 @@
 
 namespace Icinga\Module\Kubernetes\Web;
 
+use Icinga\Module\Icingadb\Util\PluginOutput;
+use Icinga\Module\Icingadb\Widget\PluginOutputContainer;
 use Icinga\Module\Kubernetes\Common\BaseListItem;
-use Icinga\Module\Kubernetes\Common\Icons;
 use Icinga\Module\Kubernetes\Common\Links;
 use Icinga\Module\Kubernetes\Common\States;
 use Icinga\Module\Kubernetes\Model\DaemonSet;
+use Icinga\Module\Kubernetes\Widget\ItemCountIndicator;
 use ipl\Html\Attributes;
 use ipl\Html\BaseHtmlElement;
 use ipl\Html\Html;
 use ipl\Html\HtmlElement;
+use ipl\Html\Text;
 use ipl\Stdlib\Str;
+use ipl\Web\Widget\HorizontalKeyValue;
 use ipl\Web\Widget\Icon;
 use ipl\Web\Widget\Link;
-use ipl\Web\Widget\StateBall;
 use ipl\Web\Widget\TimeAgo;
-use ipl\Web\Widget\VerticalKeyValue;
 
 class DaemonSetListItem extends BaseListItem
 {
+    const UPDATE_STRATEGY_ICONS = [
+        'rollingupdate' => 'repeat',
+        'recreate' => 'recycle'
+    ];
+
     /** @var $item DaemonSet The associated list item */
     /** @var $list DaemonSetList The list where the item is part of */
 
@@ -33,15 +40,63 @@ class DaemonSetListItem extends BaseListItem
     {
         $content = Html::sprintf(
             t('%s is %s', '<daemon_set> is <health>'),
-            new Link(
-                $this->item->name,
-                Links::daemonSet($this->item),
-                ['class' => 'subject']
-            ),
+            [
+                HtmlElement::create(
+                    'span',
+                    new Attributes(['class' => 'badge']),
+                    [
+                        new Icon('folder-open'),
+                        new Text($this->item->namespace)
+                    ]
+                ),
+                new Link(
+                    $this->item->name,
+                    Links::daemonSet($this->item),
+                    ['class' => 'subject']
+                )
+            ],
             Html::tag('span', ['class' => 'statefulset-text'], $this->getHealth())
         );
 
         $title->addHtml($content);
+    }
+
+    protected function assembleFooter(BaseHtmlElement $footer): void
+    {
+        $unknown = 0;
+        $available = $this->item->number_available;
+        $pending = 0;
+        $critical = $this->item->number_unavailable;
+
+        $pods = new ItemCountIndicator(null, 'hexagon');
+        for ($i = 0; $i < $critical; $i++) {
+            $pods->addItem('critical');
+        }
+        for ($i = 0; $i < $pending; $i++) {
+            $pods->addItem('pending');
+        }
+        for ($i = 0; $i < $unknown; $i++) {
+            $pods->addItem('unknown');
+        }
+        for ($i = 0; $i < $available; $i++) {
+            $pods->addItem('ok');
+        }
+        $footer->add((new HorizontalKeyValue(new Icon('box'), $pods))->addAttributes([
+            'class' => 'pods-value'
+        ]));
+
+        $footer->add(
+            (
+                new Icon(self::UPDATE_STRATEGY_ICONS[strtolower(Str::camel($this->item->update_strategy))])
+            )->addAttributes([
+                'title' => 'Update Strategy: ' . ucwords(str_replace('_', ' ', ($this->item->update_strategy)))
+            ])
+        );
+
+
+        $kvMinReadySecs = new HorizontalKeyValue(new Icon('stopwatch'), $this->item->min_ready_seconds);
+        $kvMinReadySecs->addAttributes(['title' => 'Min Ready Seconds: ' . $this->item->min_ready_seconds]);
+        $footer->add($kvMinReadySecs);
     }
 
     protected function assembleHeader(BaseHtmlElement $header): void
@@ -54,31 +109,9 @@ class DaemonSetListItem extends BaseListItem
     {
         $main->add($this->createHeader());
 
+        $main->add($this->createCaption());
 
-        $keyValue = new HtmlElement('div', new Attributes(['class' => 'key-value']));
-        $main->addHtml($keyValue);
-
-        $unknown = 0;
-        $available = $this->item->number_available;
-        $pending = 0;
-        $critical = $this->item->number_unavailable;
-        $pods = new HtmlElement('div', new Attributes(['class' => 'pod-balls']));
-        for ($i = 0; $i < $critical; $i++) {
-            $pods->addHtml(new StateBall('critical', StateBall::SIZE_MEDIUM));
-        }
-        for ($i = 0; $i < $pending; $i++) {
-            $pods->addHtml(new StateBall('pending', StateBall::SIZE_MEDIUM));
-        }
-        for ($i = 0; $i < $unknown; $i++) {
-            $pods->addHtml(new StateBall('unknown', StateBall::SIZE_MEDIUM));
-        }
-        for ($i = 0; $i < $available; $i++) {
-            $pods->addHtml(new StateBall('ok', StateBall::SIZE_MEDIUM));
-        }
-        $keyValue->add(new VerticalKeyValue('Pods', $pods));
-        $keyValue->add(new VerticalKeyValue('Update Strategy', ucfirst(Str::camel($this->item->update_strategy))));
-        $keyValue->add(new VerticalKeyValue('Min Ready Seconds', $this->item->min_ready_seconds));
-        $keyValue->add(new VerticalKeyValue('Namespace', $this->item->namespace));
+        $main->add($this->createFooter());
     }
 
     protected function getHealth(): string
@@ -95,5 +128,10 @@ class DaemonSetListItem extends BaseListItem
             default:
                 return States::HEALTHY;
         }
+    }
+
+    protected function assembleCaption(BaseHtmlElement $caption)
+    {
+        $caption->add(new PluginOutputContainer(new PluginOutput('Some carefully chosen text that gives insights about the daemon set\'s condition')));
     }
 }
