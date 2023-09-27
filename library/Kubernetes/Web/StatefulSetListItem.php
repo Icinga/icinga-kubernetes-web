1caf03295a322e501b34b7e7c8e61c96b66c875e
Florian Strohmaier <florian.strohmaier@icinga.com>
2023-09-27 17:23:10 +0200

    StatefulSetListItem: Update design

--- library/Kubernetes/Web/StatefulSetListItem.php
+++ library/Kubernetes/Web/StatefulSetListItem.php
@@ -6,11 +6,15 @@ use Icinga\Module\Kubernetes\Common\BaseListItem;
 use Icinga\Module\Kubernetes\Common\Icons;
 use Icinga\Module\Kubernetes\Common\Links;
 use Icinga\Module\Kubernetes\Model\StatefulSet;
+use Icinga\Module\Kubernetes\Widget\ItemCountIndicator;
 use ipl\Html\Attributes;
 use ipl\Html\BaseHtmlElement;
 use ipl\Html\Html;
 use ipl\Html\HtmlElement;
+use ipl\Html\Text;
+use ipl\I18n\Translation;
 use ipl\Stdlib\Str;
+use ipl\Web\Widget\HorizontalKeyValue;
 use ipl\Web\Widget\Icon;
 use ipl\Web\Widget\Link;
 use ipl\Web\Widget\StateBall;
@@ -20,6 +24,18 @@ use LogicException;
 
 class StatefulSetListItem extends BaseListItem
 {
+    use Translation;
+
+    const UPDATE_STRATEGY_ICONS = [
+        'rollingupdate' => 'repeat',
+        'recreate' => 'recycle'
+    ];
+
+    const MANAGEMENT_POLICY_ICONS = [
+        'orderedready' => 'shuffle',
+        'parallel' => 'right-left'
+    ];
+
     /** @var $item StatefulSet The associated list item */
     /** @var $list StatefulSetList The list where the item is part of */
 
@@ -33,11 +49,21 @@ class StatefulSetListItem extends BaseListItem
     {
         $content = Html::sprintf(
             t('%s is %s', '<stateful_set> is <health>'),
-            new Link(
-                $this->item->name,
-                Links::statefulSet($this->item),
-                ['class' => 'subject']
-            ),
+            [
+                HtmlElement::create(
+                    'span',
+                    new Attributes(['class' => 'badge']),
+                    [
+                        new Icon('folder-open'),
+                        new Text($this->item->namespace)
+                    ]
+                ),
+                new Link(
+                    $this->item->name,
+                    Links::statefulSet($this->item),
+                    ['class' => 'subject']
+                )
+            ],
             Html::tag('span', ['class' => 'statefulset-text'], $this->getState())
         );
 
@@ -54,33 +80,69 @@ class StatefulSetListItem extends BaseListItem
     {
         $main->add($this->createHeader());
 
-        $keyValue = new HtmlElement('div', new Attributes(['class' => 'key-value']));
-        $main->addHtml($keyValue);
+        $main->add($this->createCaption());
 
+        $main->add($this->createFooter());
+    }
+
+    protected function assembleFooter(BaseHtmlElement $footer): void
+    {
         $desired = $this->item->desired_replicas;
         $unknown = $desired - $this->item->actual_replicas;
         $available = $this->item->available_replicas;
         $pending = $available - $this->item->ready_replicas;
         $critical = $desired - $unknown - $available - $pending;
-        $pods = new HtmlElement('div', new Attributes(['class' => 'pod-balls']));
+
+        $pods = new ItemCountIndicator(null, 'hexagon');
+        $podCount = 0;
         for ($i = 0; $i < $critical; $i++) {
-            $pods->addHtml(new StateBall('critical', StateBall::SIZE_MEDIUM));
+            $pods->addItem('critical');
+            $podCount++;
         }
         for ($i = 0; $i < $pending; $i++) {
-            $pods->addHtml(new StateBall('pending', StateBall::SIZE_MEDIUM));
+            $pods->addItem('pending');
+            $podCount++;
         }
         for ($i = 0; $i < $unknown; $i++) {
-            $pods->addHtml(new StateBall('unknown', StateBall::SIZE_MEDIUM));
+            $pods->addItem('unknown');
+            $podCount++;
         }
         for ($i = 0; $i < $available; $i++) {
-            $pods->addHtml(new StateBall('ok', StateBall::SIZE_MEDIUM));
+            $pods->addItem('ok');
+            $podCount++;
         }
-        $keyValue->add(new VerticalKeyValue('Pods', $pods));
-        $keyValue->add(new VerticalKeyValue('Service Name', $this->item->service_name));
-        $keyValue->add(new VerticalKeyValue('Management Policy', ucfirst(Str::camel($this->item->pod_management_policy))));
-        $keyValue->add(new VerticalKeyValue('Update Strategy', ucfirst(Str::camel($this->item->update_strategy))));
-        $keyValue->add(new VerticalKeyValue('Min Ready Seconds', $this->item->min_ready_seconds));
-        $keyValue->add(new VerticalKeyValue('Namespace', $this->item->namespace));
+        $footer->add((
+                new HorizontalKeyValue(new Icon('box'), $pods))->addAttributes(
+                    [
+                        'title' => $podCount . '(' . $critical + $unknown . ') ' . $this->translatePlural('Pod', 'Pods', $podCount),
+                        'class'  => 'pods-value'
+                    ]
+            ));
+        $footer->add((
+                new Icon(new Icon(self::MANAGEMENT_POLICY_ICONS[strtolower(Str::camel($this->item->pod_management_policy))]))
+            )->addAttributes(
+            [
+                'title' => 'Management Policy: ' . ucwords(str_replace('_', ' ', $this->item->pod_management_policy))
+            ])
+        );
+
+        $footer->add((
+            new Icon(self::UPDATE_STRATEGY_ICONS[strtolower(Str::camel($this->item->update_strategy))])
+        )->addAttributes([
+                'title' => 'Update Strategy: ' . ucwords(str_replace('_', ' ', ($this->item->update_strategy)))
+            ]));
+        $footer->add((new HorizontalKeyValue(new Icon('stopwatch'), $this->item->min_ready_seconds))->addAttributes(
+            [
+                'title' => 'Min Ready Seconds',
+                'class' => 'push-right'
+            ]
+        ));
+
+        $footer->add((new HorizontalKeyValue(new Icon('cogs'), $this->item->service_name))->addAttributes(
+            [
+                'title' => 'Service  Name',
+            ]
+        ));
     }
 
     protected function getHealthIcon(): string
