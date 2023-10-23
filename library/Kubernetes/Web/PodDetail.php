e857910cf85c72a958103be347a8197bf8f7d5a4
Florian Strohmaier <florian.strohmaier@icinga.com>
2023-10-23 16:43:23 +0200

    PodDetail: Add icons to horizontal key values

--- library/Kubernetes/Web/PodDetail.php
+++ library/Kubernetes/Web/PodDetail.php
@@ -16,9 +16,16 @@ use ipl\Html\Text;
 use ipl\Stdlib\Filter;
 use ipl\Stdlib\Str;
 use ipl\Web\Widget\HorizontalKeyValue;
+use ipl\Web\Widget\Icon;
 
 class PodDetail extends BaseHtmlElement
 {
+    const QOS_ICONS = [
+        'best_effort' => 'award',
+        'burstable' => 'wand-magic-sparkles',
+        'guaranteed' => 'helmet-safety'
+    ];
+
     /** @var Pod */
     protected $pod;
 
@@ -39,15 +46,26 @@ class PodDetail extends BaseHtmlElement
         $details->addHtml(new HtmlElement('h2', null, new Text('Details')));
         $details->addHtml(new HorizontalKeyValue(t('Name'), $this->pod->name));
         $details->addHtml(new HorizontalKeyValue(t('IP'), $this->pod->ip));
-        $details->addHtml(new HorizontalKeyValue(t('Namespace'), $this->pod->namespace));
-        $details->addHtml(new HorizontalKeyValue(t('Node'), $this->pod->node_name));
-        $details->addHtml(new HorizontalKeyValue(t('QoS Class'), ucfirst(Str::camel($this->pod->qos))));
+        $details->addHtml(new HorizontalKeyValue(t('Namespace'),  HtmlElement::create(
+            'span',
+            new Attributes(['class' => 'namespace-badge']),
+            [
+                new Icon('folder-open'),
+                new Text($this->pod->namespace)
+            ]
+        )));
+        $details->addHtml(new HorizontalKeyValue(t('Node'), [
+            new Icon('share-nodes'),
+            $this->pod->node_name
+        ]));
+        $details->addHtml(new HorizontalKeyValue(t('QoS Class'), [ new Icon(self::QOS_ICONS[strtolower($this->pod->qos)]), ucfirst(Str::camel($this->pod->qos)) ]));
         $details->addHtml(new HorizontalKeyValue(t('Restart Policy'), ucfirst(Str::camel($this->pod->restart_policy))));
         $details->addHtml(new HorizontalKeyValue(t('Created'), $this->pod->created->format('Y-m-d H:i:s')));
 
         $this->addHtml(
             $details,
             new Labels($this->pod->label),
+            new HtmlElement('h2', null, new Text(t('Conditions'))),
             new ConditionTable($this->pod, (new PodCondition())->getColumnDefinitions()),
             new HtmlElement('h2', null, new Text('Containers')),
             new ContainerList($this->pod->container),
