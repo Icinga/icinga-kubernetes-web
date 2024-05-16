bc8d1e438850542096013aa749b4fe48642118ab
Florian Strohmaier <florian.strohmaier@icinga.com>
2024-05-16 10:15:21 +0200

    PodController: Add header controls

--- application/controllers/PodController.php
+++ application/controllers/PodController.php
@@ -7,9 +7,14 @@ namespace Icinga\Module\Kubernetes\Controllers;
 use Icinga\Module\Kubernetes\Common\Database;
 use Icinga\Module\Kubernetes\Model\Pod;
 use Icinga\Module\Kubernetes\Web\PodDetail;
+use ipl\Html\Attributes;
+use ipl\Html\HtmlElement;
+use ipl\Html\Text;
 use ipl\Stdlib\Filter;
 use ipl\Web\Compat\CompatController;
 use Ramsey\Uuid\Uuid;
+use ipl\Web\Widget\StateBall;
+use ipl\Web\Widget\TimeAgo;
 
 class PodController extends CompatController
 {
@@ -28,6 +33,21 @@ class PodController extends CompatController
         if ($pod === null) {
             $this->httpNotFound($this->translate('Pod not found'));
         }
+
+        $this->addControl(
+            new HtmlElement('div', new Attributes(['class' => 'detail-header']),
+                new StateBall('critical'),
+                new Text($pod->namespace),
+                new Text(' / '),
+                new Text($pod->name),
+                new Text($pod->created->format('Y-m-d H:i:s')),
+                new HtmlElement('br'),
+                new Text($pod->uid),
+                new Text(' / '),
+                new Text($pod->resource_version),
+                new HtmlElement('hr')
+            )
+        );
 
         $this->addContent(new PodDetail($pod));
     }
