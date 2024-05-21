096d0d655367eb1edd4f98fc1308c49eddaf149e
Florian Strohmaier <florian.strohmaier@icinga.com>
2024-05-21 15:49:34 +0200

    PodController: Add pod detail header markup

--- application/controllers/PodController.php
+++ application/controllers/PodController.php
@@ -4,8 +4,11 @@
 
 namespace Icinga\Module\Kubernetes\Controllers;
 
+use Icinga\Module\Icingadb\Model\Behavior\ActionAndNoteUrl;
+use Icinga\Module\Icingadb\Model\State;
 use Icinga\Module\Kubernetes\Common\Database;
 use Icinga\Module\Kubernetes\Model\Pod;
+use Icinga\Module\Kubernetes\Web\EndpointTable;
 use Icinga\Module\Kubernetes\Web\PodDetail;
 use ipl\Html\Attributes;
 use ipl\Html\HtmlElement;
@@ -13,8 +16,10 @@ use ipl\Html\Text;
 use ipl\Stdlib\Filter;
 use ipl\Web\Compat\CompatController;
 use Ramsey\Uuid\Uuid;
+use ipl\Web\Widget\HorizontalKeyValue;
 use ipl\Web\Widget\StateBall;
 use ipl\Web\Widget\TimeAgo;
+use ipl\Web\Widget\TimeSince;
 
 class PodController extends CompatController
 {
@@ -35,17 +40,39 @@ class PodController extends CompatController
         }
 
         $this->addControl(
-            new HtmlElement('div', new Attributes(['class' => 'detail-header']),
-                new StateBall('critical'),
-                new Text($pod->namespace),
-                new Text(' / '),
-                new Text($pod->name),
-                new Text($pod->created->format('Y-m-d H:i:s')),
-                new HtmlElement('br'),
-                new Text($pod->uid),
-                new Text(' / '),
-                new Text($pod->resource_version),
-                new HtmlElement('hr')
+            new HtmlElement('div', new Attributes(['class' => 'resource-list item-list']),
+                new HtmlElement('div', new Attributes(['class' => 'list-item']),
+                    new HtmlElement('div', new Attributes(['class' => 'visual']),
+                        new StateBall('critical', StateBall::SIZE_MEDIUM_LARGE)),
+                    new HtmlElement('div', new Attributes(['class' => 'main']),
+                        new HtmlElement('header', null,
+                            new HtmlElement('div', new Attributes(['class' => 'title']),
+                                new HtmlElement('span', new Attributes(['class' => 'subject']),
+                                    new HtmlElement('i', new Attributes(['class' => 'ikicon-kubernetes ikicon-kubernetes-pod'])),
+                                    new Text($pod->name)
+                                ),
+                                new Text(' is '),
+                                new HtmlElement('span', new Attributes(['class' => 'state-text']), new Text($pod->phase))
+                            ),
+                            new TimeSince('1716284368')
+                        ),
+                        new HtmlElement('section', new Attributes(['class' => 'caption']),
+                            new HorizontalKeyValue('created', new Text('2024-12-23 12:24'))
+                        ),
+                        /*
+                        new HtmlElement('br'),
+                        new Text($pod->uid),
+                        new Text(' / '),
+                        new Text($pod->resource_version),
+                        */
+                        new HtmlElement('footer', null,
+                            new HtmlElement('span', new Attributes(['class' => 'badge-namespace']),
+                                new HtmlElement('i', new Attributes(['class' => 'ikicon-kubernetes ikicon-kubernetes-ns'])),
+                                new Text($pod->namespace)
+                            )
+                        )
+                    )
+                )
             )
         );
 
