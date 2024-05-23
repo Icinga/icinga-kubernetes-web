56d92d438f01c3386d46daffb1edd7fcf47778c8
Johannes Rauh <johannes.rauh@icinga.com>
2024-05-23 09:28:36 +0200

    Add classes for line charts

---
+++ library/Kubernetes/Web/LineChart.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Icinga\Module\Kubernetes\Web;
+
+use ipl\Html\BaseHtmlElement;
+use ipl\Html\HtmlElement;
+use ipl\Html\Text;
+use ipl\Html\Attributes;
+
+class LineChart extends BaseHtmlElement
+{
+    protected $tag = 'div';
+
+    protected $defaultAttributes;
+
+    protected string $values;
+
+    protected string $labels;
+
+    protected string $label;
+
+    protected string $color;
+
+    public function __construct(string $chartSizeClass, string $values, string $labels, string $label, string $color)
+    {
+        $this->defaultAttributes['class'] = $chartSizeClass;
+        $this->values = $values;
+        $this->labels = $labels;
+        $this->label = $label;
+        $this->color = $color;
+    }
+
+    protected function assemble()
+    {
+        $this->addHtml(
+            new HtmlElement(
+                'canvas',
+                new Attributes(
+                    [
+                        'class' => 'line-chart',
+                        'data-values' => $this->values,
+                        'data-labels' => $this->labels,
+                        'data-label' => $this->label,
+                        'data-color' => $this->color
+                    ]
+                )
+            )
+        );
+    }
+}
---
+++ library/Kubernetes/Web/LineChartMinified.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Icinga\Module\Kubernetes\Web;
+
+use ipl\Html\BaseHtmlElement;
+use ipl\Html\HtmlElement;
+use ipl\Html\Text;
+use ipl\Html\Attributes;
+
+class LineChartMinified extends BaseHtmlElement
+{
+    protected $tag = 'div';
+
+    protected $defaultAttributes;
+
+    protected string $values;
+
+    protected string $labels;
+
+    protected string $color;
+
+    public function __construct(string $chartSizeClass, string $values, string $labels, string $color)
+    {
+        $this->defaultAttributes['class'] = $chartSizeClass;
+        $this->values = $values;
+        $this->labels = $labels;
+        $this->color = $color;
+    }
+
+    protected function assemble()
+    {
+        $this->addHtml(
+            new HtmlElement(
+                'canvas',
+                new Attributes(
+                    [
+                        'class' => 'line-chart-minified',
+                        'data-values' => $this->values,
+                        'data-labels' => $this->labels,
+                        'data-color' => $this->color
+                    ]
+                )
+            )
+        );
+    }
+}
