4e67dc292534c7935335ee6f941a8afce998fd83
Johannes Rauh <johannes.rauh@icinga.com>
2024-05-23 09:31:11 +0200

    Add charts on appropriate selectors

--- public/js/module.js
+++ public/js/module.js
@@ -4,8 +4,6 @@
 
     "use strict";
 
-    // let Chart = require('./vendor/chart.umd.js');
-
     class IcingaKubernetes extends Icinga.EventListener {
         constructor(icinga) {
             super(icinga);
@@ -18,19 +16,26 @@
         onRendered(event, autorefresh, scripted, autosubmit) {
             let color = window.getComputedStyle(event.target).getPropertyValue('color');
             console.log(color);
-            let charts = event.target.querySelectorAll('.chart');
+            let lineCharts = event.target.querySelectorAll('.line-chart');
+            let lineChartMinifieds = event.target.querySelectorAll('.line-chart-minified');
+            let doughnutCharts = event.target.querySelectorAll('.doughnut-chart');
+            let doughnutChartStates = event.target.querySelectorAll('.doughnut-chart-states');
+            let doughnutChartRequestLimits = event.target.querySelectorAll('.doughnut-chart-request-limit');
 
-            for (let lineChart of charts) {
+            for (let lineChart of lineCharts) {
                 let labelTimestamps = lineChart.dataset.labels.split(', ');
                 let labels = [];
                 let datasets = [];
 
                 for (let labelTimestamp of labelTimestamps) {
-                    labels.push(new Date(Number(labelTimestamp)).toLocaleTimeString('it-IT', {hour: '2-digit', minute: '2-digit'}));
+                    labels.push(new Date(Number(labelTimestamp)).toLocaleTimeString('it-IT', {
+                        hour: '2-digit',
+                        minute: '2-digit'
+                    }));
 
                 }
-                for (let i in lineChart.dataset.metrics.split('; ')) {
-                    let dataStrings = lineChart.dataset.metrics.split('; ')[i].split(', ');
+                for (let i in lineChart.dataset.values.split('; ')) {
+                    let dataStrings = lineChart.dataset.values.split('; ')[i].split(', ');
                     let data = [];
 
                     for (let dataString of dataStrings) {
@@ -98,6 +103,222 @@
                     },
                 });
             }
+
+            for (let lineChartMinified of lineChartMinifieds) {
+                let labelTimestamps = lineChartMinified.dataset.labels.split(', ');
+                let labels = [];
+
+                for (let labelTimestamp of labelTimestamps) {
+                    labels.push(new Date(Number(labelTimestamp)).toLocaleTimeString('it-IT', {hour: '2-digit', minute: '2-digit'}));
+                }
+
+                new Chart(lineChartMinified, {
+                    type: 'line',
+                    data: {
+                        labels: labels,
+                        datasets: [{
+                            data: lineChartMinified.dataset.values.split(', '),
+                            borderWidth: 1,
+                            borderColor: lineChartMinified.dataset.color,
+                            fill: true,
+                            backgroundColor: lineChartMinified.dataset.color + '9f',
+                            tension: 0.1,
+                        }],
+                    },
+                    options: {
+                        plugins: {
+                            legend: {
+                                display: false,
+                            },
+                        },
+                        elements: {
+                            point: {
+                                radius: 0,
+                            },
+                        },
+                        animation: false,
+                        scales: {
+                            x: {
+                                display: false,
+                            },
+                            y: {
+                                ticks: {
+                                    maxTicksLimit: 2,
+                                },
+                            },
+                        },
+                    },
+                });
+            }
+
+            for (let doughnutChart of doughnutCharts) {
+                new Chart(doughnutChart, {
+                    type: 'doughnut',
+                    data: {
+                        labels: doughnutChart.dataset.labels.split(', '),
+                        datasets: [
+                            {
+                                label: 'Test',
+                                data: doughnutChart.dataset.values.split(', '),
+                                backgroundColor: doughnutChart.dataset.colors.split(', '),
+                                borderWidth: 0,
+                            }
+                        ],
+                    },
+                    options: {
+                        animation: false,
+                    },
+                });
+            }
+
+            for (let doughnutChartState of doughnutChartStates) {
+                let percentage = doughnutChartState.dataset.value;
+                let color;
+
+                if (percentage > 0.7) {
+                    color = doughnutChartState.dataset.colors.split(', ')[2];
+                } else if (percentage > 0.5) {
+                    color = doughnutChartState.dataset.colors.split(', ')[1];
+                } else {
+                    color = doughnutChartState.dataset.colors.split(', ')[0];
+                }
+
+                new Chart(doughnutChartState, {
+                    type: 'doughnut',
+                    data: {
+                        labels: ["Empty", doughnutChartState.dataset.label, "Fill"],
+                        datasets: [
+                            {
+                                data: [2 / 3, 0.5, 0.2, 0.3],
+                                borderWidth: 0,
+                                backgroundColor: [
+                                    '#0000',
+                                    doughnutChartState.dataset.colors.split(', ')[0],
+                                    doughnutChartState.dataset.colors.split(', ')[1],
+                                    doughnutChartState.dataset.colors.split(', ')[2],
+                                ],
+                                weight: 1.5,
+                            },
+                            {
+                                data: [1],
+                                backgroundColor: [
+                                    '#0000',
+                                ],
+                                borderWidth: 0,
+                                weight: 0.5,
+                            },
+                            {
+                                data: [2 / 3, percentage, 1 - percentage],
+                                borderWidth: 0,
+                                backgroundColor: [
+                                    '#0000',
+                                    color,
+                                    '#aaaaaa7f'
+                                ],
+                                weight: 6,
+                            }
+                        ],
+                    },
+                    options: {
+                        animation: false,
+                        rotation: 360 * 0.3,
+                        cutout: '70%',
+                        plugins: {
+                            legend: {
+                                display: false,
+                                labels: {
+                                    generateLabels: (chart) => {
+                                        const legends = [];
+                                        const {data} = chart;
+                                        data.labels.forEach((label, index) => {
+                                            if (label !== 'Empty' && label !== 'Fill') {
+                                                legends.push({
+                                                    text: label,
+                                                    fillStyle: data.datasets[0].backgroundColor[index],
+                                                    hidden: false,
+                                                });
+                                            }
+                                        });
+
+                                        return legends;
+                                    }
+                                }
+                            },
+                            tooltip: {
+                                enabled: false,
+                            },
+                        },
+                        hover: {
+                            mode: null,
+                        },
+                    },
+                    // plugins: [noData2],
+                });
+
+            }
+
+            for (let doughnutChartRequestLimit of doughnutChartRequestLimits) {
+
+                let real = doughnutChartRequestLimit.dataset.real;
+                let request = doughnutChartRequestLimit.dataset.request;
+                let limit = doughnutChartRequestLimit.dataset.limit;
+
+                new Chart(doughnutChartRequestLimit, {
+                        type: "doughnut",
+                        data: {
+                            datasets: [
+
+
+                                {
+                                    borderWidth: 0,
+                                    backgroundColor: ["#0000", "#44d982", "#aaaaaa7f"],
+                                    data: [2 / 3 * limit, request, limit - request],
+                                    thickness: [[80, 90]],
+                                    weight: 5,
+                                },
+                                {
+                                    borderWidth: 0,
+                                    backgroundColor: ["#0000"],
+                                    data: [1],
+                                    weight: 0.5,
+                                },
+                                {
+                                    borderWidth: 0,
+                                    backgroundColor: ["#0000", "#593684", "#aaaaaa7f"],
+                                    data: [2 / 3 * limit, real, limit - real],
+                                    weight: 20,
+                                },
+                                {
+                                    borderWidth: 0,
+                                    backgroundColor: ["#0000"],
+                                    data: [1],
+                                    weight: 0.5,
+                                },
+                                {
+                                    borderWidth: 0,
+                                    backgroundColor: ["#0000", "#7eadff", "#aaaaaa7f"],
+                                    data: [2 / 3 * limit, limit, 0],
+                                    weight: 5,
+                                },
+                            ],
+                        },
+                        options: {
+                            plugins: {
+                                tooltip: {
+                                    enabled: false,
+                                },
+                            },
+                            cutout: "50%",
+                            hover: {
+                                mode: null,
+                            },
+                            rotation: 360 * 0.3,
+                        },
+                        // plugins: [noData2],
+                    }
+                );
+            }
+
             console.log("Charts rendered");
         }
     }
