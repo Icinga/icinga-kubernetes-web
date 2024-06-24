5668ddb55a6d77c8e113be82814172fc17d689e8
Jonada Hoxha <jonada.hoxha@icinga.com>
2024-06-24 16:20:03 +0200

    PodDetail: Remove condition-list markup

--- library/Kubernetes/Web/PodDetail.php
+++ library/Kubernetes/Web/PodDetail.php
@@ -22,8 +22,6 @@ use ipl\Stdlib\Filter;
 use ipl\Stdlib\Str;
 use ipl\Web\Widget\CopyToClipboard;
 use ipl\Web\Widget\StateBall;
-use ipl\Web\Widget\Icon;
-use ipl\Web\Widget\TimeAgo;
 
 class PodDetail extends BaseHtmlElement
 {
@@ -99,60 +97,6 @@ class PodDetail extends BaseHtmlElement
                                 new HtmlElement('i', new Attributes(['class' => 'ikicon-kubernetes ikicon-kubernetes-ingress'])),
                                 new Text(' '),
                                 new Text('rs-name-random')
-                            )
-                        )
-                    )
-                )
-            ),
-            /* new ConditionTable($this->pod, (new PodCondition())->getColumnDefinitions()),*/
-            new HtmlElement(
-                'section',
-                null,
-                new HtmlElement('h2', null, new Text('Conditions')),
-                new HtmlElement('ul', new Attributes(['class' => 'condition-list item-list']),
-                    new HtmlElement('li', new Attributes(['class' => 'list-item']),
-                        new HtmlElement('div', new Attributes(['class' => 'visual inactive']),
-                            (new Icon('circle'))->setStyle('fa-regular')
-                        ),
-                        new HtmlElement('div', new Attributes(['class' => 'main']),
-                            new HtmlElement('header', null,
-                                new HtmlElement('h3', null, new Text('Ready'))
-                            )
-                        )
-                    ),
-                    new HtmlElement('li', new Attributes(['class' => 'list-item']),
-                        new HtmlElement('div', new Attributes(['class' => 'visual error']),
-                            new Icon('times-circle')
-                        ),
-                        new HtmlElement('div', new Attributes(['class' => 'main']),
-                            new HtmlElement('header', null,
-                                new HtmlElement('h3', null, new Text('Containers Ready')),
-                                new TimeAgo(1715752000)
-                            ),
-                            new HtmlElement('section', new Attributes(['class' => 'caption']),
-                                new PluginOutputContainer(new PluginOutput('Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas faucibus mollis interdum. Maecenas faucibus mollis interdum. Etiam porta sem malesuada magna mollis euismod. Sed posuere consectetur est at lobortis.'))
-                            )
-                        )
-                    ),
-                    new HtmlElement('li', new Attributes(['class' => 'list-item']),
-                        new HtmlElement('div', new Attributes(['class' => 'visual success']),
-                            (new Icon('check-circle'))->setStyle('fa-regular')
-                        ),
-                        new HtmlElement('div', new Attributes(['class' => 'main']),
-                            new HtmlElement('header', null,
-                                new HtmlElement('h3', null, new Text('Scheduled')),
-                                new TimeAgo(1715740000)
-                            )
-                        )
-                    ),
-                    new HtmlElement('li', new Attributes(['class' => 'list-item']),
-                        new HtmlElement('div', new Attributes(['class' => 'visual success']),
-                            (new Icon('check-circle'))->setStyle('fa-regular')
-                        ),
-                        new HtmlElement('div', new Attributes(['class' => 'main']),
-                            new HtmlElement('header', null,
-                                new HtmlElement('h3', null, new Text('Initialized')),
-                                new TimeAgo(1715690000)
                             )
                         )
                     )
