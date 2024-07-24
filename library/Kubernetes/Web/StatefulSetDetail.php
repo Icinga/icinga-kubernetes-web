3102f90b31a519875d7e059794526af83cef4fb3
Eric Lippmann <eric.lippmann@icinga.com>
2024-07-24 15:29:42 +0200

    statefulset

--- library/Kubernetes/Web/StatefulSetDetail.php
+++ library/Kubernetes/Web/StatefulSetDetail.php
@@ -13,34 +13,40 @@ use ipl\Html\HtmlDocument;
 use ipl\Html\HtmlElement;
 use ipl\Html\Text;
 use ipl\I18n\Translation;
-use ipl\Stdlib\Str;
+use ipl\Web\Widget\Icon;
 use ipl\Web\Widget\StateBall;
 
 class StatefulSetDetail extends BaseHtmlElement
 {
     use Translation;
 
-    /** @var StatefulSet */
-    protected $statefulSet;
+    protected StatefulSet $statefulSet;
 
     protected $tag = 'div';
 
-    public function __construct($statefulSet)
+    protected $defaultAttributes = ['class' => 'stateful-set-detail'];
+
+    public function __construct(StatefulSet $statefulSet)
     {
         $this->statefulSet = $statefulSet;
     }
 
-    protected function assemble()
+    protected function assemble(): void
     {
         $this->addHtml(
             new Details(new ResourceDetails($this->statefulSet, [
-                $this->translate('Service Name')          => $this->statefulSet->service_name,
-                $this->translate('Pod Management Policy') => ucfirst(Str::camel(
-                    $this->statefulSet->pod_management_policy
-                )),
-                $this->translate('Update Strategy')       => ucfirst(Str::camel(
-                    $this->statefulSet->update_strategy
-                )),
+                $this->translate('Pod Management Policy') => (new HtmlDocument())->addHtml(
+                    new Icon(StatefulSetListItem::MANAGEMENT_POLICY_ICONS[$this->statefulSet->pod_management_policy]),
+                    new Text($this->statefulSet->pod_management_policy)
+                ),
+                $this->translate('Update Strategy')       => (new HtmlDocument())->addHtml(
+                    new Icon(StatefulSetListItem::UPDATE_STRATEGY_ICONS[$this->statefulSet->update_strategy]),
+                    new Text($this->statefulSet->update_strategy)
+                ),
+                $this->translate('Service Name')          => (new HtmlDocument())->addHtml(
+                    new HtmlElement('i', new Attributes(['class' => 'icon kicon-service'])),
+                    new Text($this->statefulSet->service_name)
+                ),
                 $this->translate('Min Ready Seconds')     => $this->statefulSet->min_ready_seconds,
                 $this->translate('Desired Replicas')      => $this->statefulSet->desired_replicas,
                 $this->translate('Actual Replicas')       => $this->statefulSet->actual_replicas,
@@ -48,13 +54,17 @@ class StatefulSetDetail extends BaseHtmlElement
                 $this->translate('Updated Replicas')      => $this->statefulSet->updated_replicas,
                 $this->translate('Ready Replicas')        => $this->statefulSet->ready_replicas,
                 $this->translate('Available Replicas')    => $this->statefulSet->available_replicas,
-                $this->translate('Icinga State')          => (new HtmlDocument())
-                    ->addHtml(new StateBall($this->statefulSet->icinga_state, StateBall::SIZE_MEDIUM))
-                    ->addHtml(new HtmlElement('span', null, Text::create(' ' . $this->statefulSet->icinga_state))),
+                $this->translate('Icinga State')          => (new HtmlDocument())->addHtml(
+                    new StateBall($this->statefulSet->icinga_state, StateBall::SIZE_MEDIUM),
+                    new HtmlElement(
+                        'span',
+                        new Attributes(['class' => 'icinga-state-text']),
+                        new Text(' ' . $this->statefulSet->icinga_state))
+                ),
                 $this->translate('Icinga State Reason')   => new HtmlElement(
                     'div',
-                    new Attributes(['class' => 'state-reason detail']),
-                    Text::create($this->statefulSet->icinga_state_reason)
+                    new Attributes(['class' => 'icinga-state-reason']),
+                    new Text($this->statefulSet->icinga_state_reason)
                 )
             ])),
             new Labels($this->statefulSet->label),
