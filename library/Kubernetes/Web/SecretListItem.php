00dd5b1e8a167134fc27d08d7f9e34c9f9028f9b
Eric Lippmann <eric.lippmann@icinga.com>
2024-07-24 15:43:26 +0200

    Update secret list and detail markup

--- library/Kubernetes/Web/SecretListItem.php
+++ library/Kubernetes/Web/SecretListItem.php
@@ -8,11 +8,13 @@ use Icinga\Module\Kubernetes\Common\BaseListItem;
 use Icinga\Module\Kubernetes\Common\Links;
 use ipl\Html\Attributes;
 use ipl\Html\BaseHtmlElement;
+use ipl\Html\HtmlDocument;
 use ipl\Html\HtmlElement;
+use ipl\Html\Text;
 use ipl\I18n\Translation;
+use ipl\Web\Widget\HorizontalKeyValue;
 use ipl\Web\Widget\Link;
 use ipl\Web\Widget\TimeAgo;
-use ipl\Web\Widget\VerticalKeyValue;
 
 class SecretListItem extends BaseListItem
 {
@@ -20,23 +22,42 @@ class SecretListItem extends BaseListItem
 
     protected function assembleHeader(BaseHtmlElement $header): void
     {
-        $header
-            ->addHtml($this->createTitle())
-            ->addHtml(new TimeAgo($this->item->created->getTimestamp()));
+        $header->addHtml(
+            $this->createTitle(),
+            new TimeAgo($this->item->created->getTimestamp())
+        );
     }
 
     protected function assembleMain(BaseHtmlElement $main): void
     {
-        $main->addHtml($this->createHeader());
+        $main->addHtml(
+            $this->createHeader(),
+            $this->createFooter()
+        );
+    }
 
-        $keyValue = new HtmlElement('div', new Attributes(['class' => 'key-value']));
-        $keyValue->addHtml(new VerticalKeyValue($this->translate('Type'), $this->item->type));
-        $keyValue->addHtml(new VerticalKeyValue($this->translate('Namespace'), $this->item->namespace));
-        $main->addHtml($keyValue);
+    protected function assembleFooter(BaseHtmlElement $footer): void
+    {
+        $footer->addHtml(new HorizontalKeyValue($this->translate('Type'), $this->item->type));
     }
 
     protected function assembleTitle(BaseHtmlElement $title): void
     {
-        $title->addHtml(new Link($this->item->name, Links::secret($this->item), ['class' => 'subject']));
+        $title->addHtml(
+            new HtmlElement(
+                'span',
+                new Attributes(['class' => 'namespace-badge']),
+                new HtmlElement('i', new Attributes(['class' => 'icon kicon-namespace'])),
+                new Text($this->item->namespace)
+            ),
+            new Link(
+                (new HtmlDocument())->addHtml(
+                    new HtmlElement('i', new Attributes(['class' => 'icon kicon-secret'])),
+                    new Text($this->item->name)
+                ),
+                Links::secret($this->item),
+                new Attributes(['class' => 'subject'])
+            )
+        );
     }
 }
