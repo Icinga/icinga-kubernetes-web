69dfb97f1917393fa87620098f8dc18fec3d33aa
Eric Lippmann <eric.lippmann@icinga.com>
2024-07-24 10:49:16 +0200

    Update daemon set list and detail markup

--- library/Kubernetes/Web/DaemonSetDetail.php
+++ library/Kubernetes/Web/DaemonSetDetail.php
@@ -13,29 +13,34 @@ use ipl\Html\HtmlDocument;
 use ipl\Html\HtmlElement;
 use ipl\Html\Text;
 use ipl\I18n\Translation;
-use ipl\Stdlib\Str;
+use ipl\Web\Widget\Icon;
 use ipl\Web\Widget\StateBall;
 
 class DaemonSetDetail extends BaseHtmlElement
 {
     use Translation;
 
-    /** @var DaemonSet */
-    protected $daemonSet;
+    protected DaemonSet $daemonSet;
 
     protected $tag = 'div';
+
+    protected $defaultAttributes = ['class' => 'daemon-set-detail'];
 
     public function __construct(DaemonSet $daemonSet)
     {
         $this->daemonSet = $daemonSet;
     }
 
-    protected function assemble()
+    protected function assemble(): void
     {
         $this->addHtml(
             new Details(new ResourceDetails($this->daemonSet, [
-                $this->translate('Update Strategy')          => ucfirst(Str::camel($this->daemonSet->update_strategy)),
-                $this->translate('Min Ready Seconds')        => $this->daemonSet->min_ready_seconds,
+                $this->translate('Update Strategy')          => (new HtmlDocument())
+                    ->addHtml(new Icon(DaemonSetListItem::UPDATE_STRATEGY_ICONS[$this->daemonSet->update_strategy]))
+                    ->addHtml(new Text($this->daemonSet->update_strategy)),
+                $this->translate('Min Ready Seconds')        => (new HtmlDocument())
+                    ->addHtml(new Icon('stopwatch'))
+                    ->addHtml(new Text($this->daemonSet->min_ready_seconds . 's')),
                 $this->translate('Desired Number Scheduled') => $this->daemonSet->desired_number_scheduled,
                 $this->translate('Current Number Scheduled') => $this->daemonSet->current_number_scheduled,
                 $this->translate('Update Number Scheduled')  => $this->daemonSet->update_number_scheduled,
@@ -45,11 +50,15 @@ class DaemonSetDetail extends BaseHtmlElement
                 $this->translate('Number Unavailable')       => $this->daemonSet->number_unavailable,
                 $this->translate('Icinga State')             => (new HtmlDocument())
                     ->addHtml(new StateBall($this->daemonSet->icinga_state, StateBall::SIZE_MEDIUM))
-                    ->addHtml(new HtmlElement('span', null, Text::create(' ' . $this->daemonSet->icinga_state))),
+                    ->addHtml(new HtmlElement(
+                        'span',
+                        new Attributes(['class' => 'icinga-state-text']),
+                        new Text($this->daemonSet->icinga_state)
+                    )),
                 $this->translate('Icinga State Reason')      => new HtmlElement(
                     'div',
-                    new Attributes(['class' => 'state-reason detail']),
-                    Text::create($this->daemonSet->icinga_state_reason)
+                    new Attributes(['class' => 'icinga-state-reason']),
+                    new Text($this->daemonSet->icinga_state_reason)
                 )
             ])),
             new Labels($this->daemonSet->label),
