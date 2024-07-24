6f6bbc11fa66a489f0c626203e606f178f95d9f8
Eric Lippmann <eric.lippmann@icinga.com>
2024-07-24 13:17:30 +0200

    Update namespace list and detail markup

--- library/Kubernetes/Web/NamespaceListItem.php
+++ library/Kubernetes/Web/NamespaceListItem.php
@@ -5,16 +5,16 @@
 namespace Icinga\Module\Kubernetes\Web;
 
 use Icinga\Module\Kubernetes\Common\BaseListItem;
-use Icinga\Module\Kubernetes\Common\Icons;
 use Icinga\Module\Kubernetes\Common\Links;
 use Icinga\Module\Kubernetes\Model\NamespaceModel;
+use ipl\Html\Attributes;
 use ipl\Html\BaseHtmlElement;
 use ipl\Html\Html;
 use ipl\Html\HtmlElement;
 use ipl\Html\Text;
 use ipl\I18n\Translation;
-use ipl\Web\Widget\Icon;
 use ipl\Web\Widget\Link;
+use ipl\Web\Widget\StateBall;
 use ipl\Web\Widget\TimeAgo;
 
 class NamespaceListItem extends BaseListItem
@@ -38,24 +38,14 @@ class NamespaceListItem extends BaseListItem
         $title->addHtml(Html::sprintf(
             $this->translate('%s is %s', '<namespace> is <namespace_phase>'),
             new Link($this->item->name, Links::namespace($this->item), ['class' => 'subject']),
-            new HtmlElement('span', null, new Text($this->item->phase))
+            new HtmlElement('span', new Attributes(['class' => 'namespace-phase']), new Text($this->item->phase))
         ));
     }
 
     protected function assembleVisual(BaseHtmlElement $visual): void
     {
-        $visual->addHtml(new Icon($this->getPhaseIcon(), ['class' => ['namespace-phase-' . $this->item->phase]]));
-    }
-
-    protected function getPhaseIcon(): string
-    {
-        switch ($this->item->phase) {
-            case NamespaceModel::PHASE_ACTIVE:
-                return Icons::NAMESPACE_ACTIVE;
-            case NamespaceModel::PHASE_TERMINATING:
-                return Icons::NAMESPACE_TERMINATING;
-            default:
-                return Icons::BUG;
+        if ($this->item->phase === NamespaceModel::PHASE_ACTIVE) {
+            $visual->addHtml(new StateBall('ok', StateBall::SIZE_MEDIUM));
         }
     }
 }
