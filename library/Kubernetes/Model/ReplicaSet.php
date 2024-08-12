c76c222a4d7136e558d14d1cfebbf66cd94f9eb1
Eric Lippmann <eric.lippmann@icinga.com>
2024-08-12 16:19:50 +0200

    Update replica set list and detail markup

--- library/Kubernetes/Model/ReplicaSet.php
+++ library/Kubernetes/Model/ReplicaSet.php
@@ -72,7 +72,7 @@ class ReplicaSet extends Model
             'available_replicas'     => $this->translate('Available Replicas'),
             'icinga_state'           => $this->translate('Icinga State'),
             'icinga_state_reason'    => $this->translate('Icinga State Reason'),
-            'yaml'                  => $this->translate('YAML'),
+            'yaml'                   => $this->translate('YAML'),
             'created'                => $this->translate('Created At')
         ];
     }
--- library/Kubernetes/Web/ReplicaSetDetail.php
+++ library/Kubernetes/Web/ReplicaSetDetail.php
@@ -5,6 +5,7 @@
 namespace Icinga\Module\Kubernetes\Web;
 
 use Icinga\Module\Kubernetes\Common\Database;
+use Icinga\Module\Kubernetes\Common\Format;
 use Icinga\Module\Kubernetes\Common\ResourceDetails;
 use Icinga\Module\Kubernetes\Model\Event;
 use Icinga\Module\Kubernetes\Model\ReplicaSet;
@@ -16,39 +17,47 @@ use ipl\Html\HtmlElement;
 use ipl\Html\Text;
 use ipl\I18n\Translation;
 use ipl\Stdlib\Filter;
+use ipl\Web\Widget\Icon;
 use ipl\Web\Widget\StateBall;
 
 class ReplicaSetDetail extends BaseHtmlElement
 {
     use Translation;
 
-    /** @var ReplicaSet */
-    protected $replicaSet;
+    protected ReplicaSet $replicaSet;
 
     protected $tag = 'div';
+
+    protected $defaultAttributes = ['class' => 'replica-set-detail'];
 
     public function __construct(ReplicaSet $replicaSet)
     {
         $this->replicaSet = $replicaSet;
     }
 
-    protected function assemble()
+    protected function assemble(): void
     {
         $this->addHtml(
             new Details(new ResourceDetails($this->replicaSet, [
-                $this->translate('Min Ready Seconds')      => $this->replicaSet->min_ready_seconds,
+                $this->translate('Min Ready Duration')     => (new HtmlDocument())->addHtml(
+                    new Icon('stopwatch'),
+                    new Text(Format::seconds($this->replicaSet->min_ready_seconds, $this->translate('None')))
+                ),
                 $this->translate('Desired Replicas')       => $this->replicaSet->desired_replicas,
                 $this->translate('Actual Replicas')        => $this->replicaSet->actual_replicas,
                 $this->translate('Fully Labeled Replicas') => $this->replicaSet->fully_labeled_replicas,
                 $this->translate('Ready Replicas')         => $this->replicaSet->ready_replicas,
                 $this->translate('Available Replicas')     => $this->replicaSet->available_replicas,
-                $this->translate('Icinga State')           => (new HtmlDocument())
-                    ->addHtml(new StateBall($this->replicaSet->icinga_state, StateBall::SIZE_MEDIUM))
-                    ->addHtml(new HtmlElement('span', null, Text::create(' ' . $this->replicaSet->icinga_state))),
-                $this->translate('Icinga State Reason')    => new HtmlElement(
-                    'div',
-                    new Attributes(['class' => 'state-reason detail']),
-                    Text::create($this->replicaSet->icinga_state_reason)
+                $this->translate('Icinga State')           => (new HtmlDocument())->addHtml(
+                    new StateBall($this->replicaSet->icinga_state, StateBall::SIZE_MEDIUM),
+                    new HtmlElement(
+                        'span',
+                        new Attributes(['class' => 'icinga-state-text']),
+                        Text::create($this->replicaSet->icinga_state)
+                    )
+                ),
+                $this->translate('Icinga State Reason')    => new IcingaStateReason(
+                    $this->replicaSet->icinga_state_reason
                 )
             ])),
             new Labels($this->replicaSet->label),
@@ -66,13 +75,7 @@ class ReplicaSetDetail extends BaseHtmlElement
                 new HtmlElement('h2', null, new Text($this->translate('Events'))),
                 new EventList(
                     Event::on(Database::connection())
-                        ->filter(
-                            Filter::all(
-                                Filter::equal('reference_kind', 'ReplicaSet'),
-                                Filter::equal('reference_namespace', $this->replicaSet->namespace),
-                                Filter::equal('reference_name', $this->replicaSet->name)
-                            )
-                        )
+                        ->filter(Filter::equal('referent_uuid', $this->replicaSet->uuid))
                 )
             ),
             new Yaml($this->replicaSet->yaml)
