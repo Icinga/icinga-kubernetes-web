56748c1ca45ab11e8a2f56895d68a089a21cd665
Eric Lippmann <eric.lippmann@icinga.com>
2024-08-12 16:02:29 +0200

    Introduce function `yield_iterable()`

---
+++ run.php
@@ -0,0 +1,19 @@
+<?php
+
+/* Icinga for Kubernetes Web | (c) 2024 Icinga GmbH | AGPLv3 */
+
+if (! function_exists('yield_iterable')) {
+    /**
+     * Turn any iterable into a generator
+     *
+     * @param iterable $iterable
+     *
+     * @return Generator
+     */
+    function yield_iterable(iterable $iterable): Generator
+    {
+        foreach ($iterable as $k => $v) {
+            yield $k => $v;
+        }
+    }
+}
