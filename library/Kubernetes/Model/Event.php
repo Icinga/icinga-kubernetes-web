4539ee2bf1ccf2b23035a4ede5e904a827e78574
Eric Lippmann <eric.lippmann@icinga.com>
2024-08-22 10:00:17 +0200

    `Events`: Add `Column Definitions`

--- library/Kubernetes/Model/Event.php
+++ library/Kubernetes/Model/Event.php
@@ -5,6 +5,7 @@
 namespace Icinga\Module\Kubernetes\Model;
 
 use Icinga\Module\Kubernetes\Model\Behavior\Uuid;
+use ipl\I18n\Translation;
 use ipl\Orm\Behavior\MillisecondTimestamp;
 use ipl\Orm\Behaviors;
 use ipl\Orm\Model;
@@ -12,6 +13,8 @@ use ipl\Orm\Relations;
 
 class Event extends Model
 {
+    use Translation;
+
     public function createBehaviors(Behaviors $behaviors)
     {
         $behaviors->add(new Uuid([
@@ -28,6 +31,29 @@ class Event extends Model
 
     public function createRelations(Relations $relations)
     {
+    }
+
+    public function getColumnDefinitions(): array
+    {
+        return [
+            'namespace'            => $this->translate('Namespace'),
+            'name'                 => $this->translate('Name'),
+            'uid'                  => $this->translate('UID'),
+            'resource_version'     => $this->translate('Resource Version'),
+            'reporting_controller' => $this->translate('Reporting Controller'),
+            'reporting_instance'   => $this->translate('Reporting Instance'),
+            'action'               => $this->translate('Action'),
+            'reason'               => $this->translate('Reason'),
+            'note'                 => $this->translate('Note'),
+            'type'                 => $this->translate('Type'),
+            'reference_kind'       => $this->translate('Referent Kind'),
+            'reference_namespace'  => $this->translate('Referent Namespace'),
+            'reference_name'       => $this->translate('Referent Name'),
+            'first_seen'           => $this->translate('First Seen'),
+            'last_seen'            => $this->translate('Last Seen'),
+            'count'                => $this->translate('Count'),
+            'created'              => $this->translate('Created')
+        ];
     }
 
     public function getColumns()
