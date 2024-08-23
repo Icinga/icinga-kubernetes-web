098c026fc65568078500e8ac8ea66546edfbc638
Johannes Rauh <johannes.rauh@icinga.com>
2024-08-23 11:16:03 +0200

    Make Prometheus credentials configurable via web

--- application/controllers/ConfigController.php
+++ application/controllers/ConfigController.php
@@ -28,6 +28,12 @@ class ConfigController extends Controller
 {
     protected bool $disableDefaultAutoRefresh = true;
 
+    public const PROMETHEUS_URL = 'prometheus.url';
+
+    public const PROMETHEUS_USERNAME = 'prometheus.username';
+
+    public const PROMETHEUS_PASSWORD = 'prometheus.password';
+
     public function init()
     {
         $this->assertPermission('config/modules');
@@ -181,34 +187,81 @@ class ConfigController extends Controller
     public function prometheusAction()
     {
         $db = Database::connection();
-        $dbConfig = KConfig::on($db)->filter(Filter::equal('key', 'prometheus.url'))->first();
+        $dbConfig = KConfig::on($db)->filter(
+            Filter::any(
+                Filter::equal('key', self::PROMETHEUS_URL),
+                Filter::equal('key', self::PROMETHEUS_USERNAME),
+                Filter::equal('key', self::PROMETHEUS_PASSWORD)
+            )
+        );
 
-//        $config = Config::module('kubernetes');
+        $data = [];
+
+        foreach ($dbConfig as $pair) {
+            switch ($pair->key) {
+                case self::PROMETHEUS_URL:
+                    $data['prometheus_url'] = $pair->value;
+                    break;
+                case self::PROMETHEUS_USERNAME:
+                    $data['prometheus_username'] = $pair->value;
+                    break;
+                case self::PROMETHEUS_PASSWORD:
+                    $data['prometheus_password'] = $pair->value;
+                    break;
+            }
+        }
+
         $form = (new PrometheusConfigForm())
-            ->populate(['prometheus_url' => $dbConfig->value])
-            ->on(PrometheusConfigForm::ON_SUCCESS, function ($form) use ($db, $dbConfig) {
+            ->populate($data)
+            ->on(PrometheusConfigForm::ON_SUCCESS, function ($form) use ($db, $data) {
                 if ($form->isLocked()) {
                     Notification::error($this->translate('Prometheus configuration is locked'));
                     return;
                 }
 
-//                $config->setSection('prometheus', $form->getValues());
-//                $config->saveIni();
-
-                if ($dbConfig) {
+                if (isset($data['prometheus_url'])) {
                     $db->update('config',
                         ['value' => $form->getValue('prometheus_url')],
-                        [$db->quoteIdentifier('key') . ' = ?' => 'prometheus.url']
+                        [$db->quoteIdentifier('key') . ' = ?' => self::PROMETHEUS_URL]
                     );
                 } else {
                     $db->insert('config',
                         [
-                            $db->quoteIdentifier('key') => 'prometheus.url',
+                            $db->quoteIdentifier('key') => self::PROMETHEUS_URL,
                             'value'                     => $form->getValue('prometheus_url')
                         ]
                     );
                 }
 
+                if (isset($data['prometheus_username'])) {
+                    $db->update('config',
+                        ['value' => $form->getValue('prometheus_username')],
+                        [$db->quoteIdentifier('key') . ' = ?' => self::PROMETHEUS_USERNAME]
+                    );
+                } else {
+                    $db->insert('config',
+                        [
+                            $db->quoteIdentifier('key') => self::PROMETHEUS_USERNAME,
+                            'value'                     => $form->getValue('prometheus_username')
+                        ]
+                    );
+                }
+
+                if (isset($data['prometheus_password'])) {
+                    $db->update('config',
+                        ['value' => $form->getValue('prometheus_password')],
+                        [$db->quoteIdentifier('key') . ' = ?' => self::PROMETHEUS_PASSWORD]
+                    );
+                } else {
+                    $db->insert('config',
+                        [
+                            $db->quoteIdentifier('key') => self::PROMETHEUS_PASSWORD,
+                            'value'                     => $form->getValue('prometheus_password')
+                        ]
+                    );
+                }
+
+                $this->redirectNow('__REFRESH__');
 
                 Notification::success($this->translate('New configuration has successfully been stored'));
             })->handleRequest($this->getServerRequest());
