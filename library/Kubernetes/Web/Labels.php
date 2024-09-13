862bb227c7bf377b3ff962545f654e4a3ed09512
Jonada Hoxha <jonada.hoxha@icinga.com>
2024-09-13 11:07:08 +0200

    Labels: Enhance label rendering with structured titles and nested list items

--- library/Kubernetes/Web/Labels.php
+++ library/Kubernetes/Web/Labels.php
@@ -4,10 +4,12 @@
 
 namespace Icinga\Module\Kubernetes\Web;
 
+use ipl\Html\Attributes;
 use ipl\Html\BaseHtmlElement;
 use ipl\Html\HtmlElement;
 use ipl\Html\Text;
 use ipl\I18n\Translation;
+use ipl\Stdlib\Str;
 use ipl\Web\Widget\EmptyState;
 use ipl\Web\Widget\HorizontalKeyValue;
 
@@ -33,12 +35,60 @@ class Labels extends BaseHtmlElement
         $this->addHtml(new HtmlElement('h2', null, new Text($this->translate('Labels'))));
 
         $labels = yield_iterable($this->labels);
+        $content = [];
         if ($labels->valid()) {
-            foreach ($labels as $label) {
-                $this->addHtml(new HorizontalKeyValue($label->name, $label->value));
+            foreach ($this->categorizeLabels($labels) as $title => $labels) {
+                $content[] = new HtmlElement(
+                    'li',
+                    null,
+                    new HtmlElement(
+                        'span',
+                        new Attributes(['class' => 'title']),
+                        new Text($title)
+                    ),
+                    new HtmlElement(
+                        'ul',
+                        null,
+                        ...array_map(
+                            fn($name, $value) => new HtmlElement(
+                                'li',
+                                null,
+                                new HorizontalKeyValue($name, $value)
+                            ),
+                            array_keys($labels),
+                            $labels
+                        )
+                    )
+                );
             }
+
+            $this->addHtml(
+                new HtmlElement(
+                    'div',
+                    new Attributes([
+                        'class'               => 'collapsible',
+                        'data-visible-height' => 100
+                    ]),
+                    new HtmlElement('ul', null, ...$content)
+                )
+            );
         } else {
             $this->addHtml(new EmptyState($this->translate('No items to display.')));
         }
+    }
+
+    private function categorizeLabels(iterable $labels): array
+    {
+        $categorized = [];
+
+        foreach ($labels as $label) {
+            [$prefix, $name] = Str::symmetricSplit($label->name, '/', 2);
+            $group = $name ? $prefix : '-';
+            $categorized[$group][$name ?: $prefix] = $label->value;
+        }
+
+        uksort($categorized, fn($a, $b) => $a === '-' ? 1 : ($b === '-' ? -1 : strcmp($a, $b)));
+
+        return $categorized;
     }
 }
