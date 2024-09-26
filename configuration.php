e14de87b1a0f63b6c44740b756421dd6166af49e
Johannes Rauh <johannes.rauh@icinga.com>
2024-09-26 14:50:05 +0200

    Sort menu alphabetically

--- configuration.php
+++ configuration.php
@@ -16,127 +16,10 @@ $section = $this->menuSection(
 $priority = 0;
 
 $section->add(
-    N_('Nodes'),
-    [
-        'description' => $this->translate('Nodes'),
-        'url'         => 'kubernetes/nodes',
-        'priority'    => $priority++
-    ]
-);
-
-$section->add(
-    N_('Namespaces'),
-    [
-        'description' => $this->translate('Namespaces'),
-        'url'         => 'kubernetes/namespaces',
-        'priority'    => $priority++
-    ]
-);
-
-$section->add(
-    N_('Deployments'),
-    [
-        'description' => $this->translate('Deployments'),
-        'url'         => 'kubernetes/deployments',
-        'priority'    => $priority++
-    ]
-);
-
-$section->add(
-    N_('Replica Sets'),
-    [
-        'description' => $this->translate('Replica Sets'),
-        'url'         => 'kubernetes/replicasets',
-        'priority'    => $priority++
-    ]
-);
-
-$section->add(
-    N_('Daemon Sets'),
-    [
-        'description' => $this->translate('Daemon Sets'),
-        'url'         => 'kubernetes/daemonsets',
-        'priority'    => $priority++
-    ]
-);
-
-$section->add(
-    N_('Stateful Sets'),
-    [
-        'description' => $this->translate('Stateful Sets'),
-        'url'         => 'kubernetes/statefulsets',
-        'priority'    => $priority++
-    ]
-);
-
-$section->add(
-    N_('Pods'),
-    [
-        'description' => $this->translate('Pods'),
-        'url'         => 'kubernetes/pods',
-        'priority'    => $priority++
-    ]
-);
-
-$section->add(
-    N_('Persistent Volumes'),
-    [
-        'description' => $this->translate('Persistent Volumes'),
-        'url'         => 'kubernetes/persistentvolumes',
-        'priority'    => $priority++
-    ]
-);
-
-$section->add(
-    N_('Persistent Volume Claims'),
-    [
-        'description' => $this->translate('Persistent Volume Claims'),
-        'url'         => 'kubernetes/persistentvolumeclaims',
-        'priority'    => $priority++
-    ]
-);
-
-$section->add(
-    N_('Cron Jobs'),
-    [
-        'description' => $this->translate('Cron Jobs'),
-        'url'         => 'kubernetes/cronjobs',
-        'priority'    => $priority++
-    ]
-);
-
-$section->add(
-    N_('Jobs'),
-    [
-        'description' => $this->translate('Jobs'),
-        'url'         => 'kubernetes/jobs',
-        'priority'    => $priority++
-    ]
-);
-
-$section->add(
-    N_('Services'),
-    [
-        'description' => $this->translate('Services'),
-        'url'         => 'kubernetes/services',
-        'priority'    => $priority++
-    ]
-);
-
-$section->add(
     N_('Cluster Services'),
     [
         'description' => $this->translate('Cluster Services'),
         'url'         => 'kubernetes/services?label.name=kubernetes.io%2Fcluster-service&label.value=true',
-        'priority'    => $priority++
-    ]
-);
-
-$section->add(
-    N_('Ingresses'),
-    [
-        'description' => $this->translate('Ingresses'),
-        'url'         => 'kubernetes/ingresses',
         'priority'    => $priority++
     ]
 );
@@ -151,10 +34,28 @@ $section->add(
 );
 
 $section->add(
-    N_('Secrets'),
+    N_('Cron Jobs'),
     [
-        'description' => $this->translate('Secrets'),
-        'url'         => 'kubernetes/secrets',
+        'description' => $this->translate('Cron Jobs'),
+        'url'         => 'kubernetes/cronjobs',
+        'priority'    => $priority++
+    ]
+);
+
+$section->add(
+    N_('Daemon Sets'),
+    [
+        'description' => $this->translate('Daemon Sets'),
+        'url'         => 'kubernetes/daemonsets',
+        'priority'    => $priority++
+    ]
+);
+
+$section->add(
+    N_('Deployments'),
+    [
+        'description' => $this->translate('Deployments'),
+        'url'         => 'kubernetes/deployments',
         'priority'    => $priority++
     ]
 );
@@ -164,6 +65,105 @@ $section->add(
     [
         'description' => $this->translate('Events'),
         'url'         => 'kubernetes/events',
+        'priority'    => $priority++
+    ]
+);
+
+$section->add(
+    N_('Ingresses'),
+    [
+        'description' => $this->translate('Ingresses'),
+        'url'         => 'kubernetes/ingresses',
+        'priority'    => $priority++
+    ]
+);
+
+$section->add(
+    N_('Jobs'),
+    [
+        'description' => $this->translate('Jobs'),
+        'url'         => 'kubernetes/jobs',
+        'priority'    => $priority++
+    ]
+);
+
+$section->add(
+    N_('Namespaces'),
+    [
+        'description' => $this->translate('Namespaces'),
+        'url'         => 'kubernetes/namespaces',
+        'priority'    => $priority++
+    ]
+);
+
+$section->add(
+    N_('Nodes'),
+    [
+        'description' => $this->translate('Nodes'),
+        'url'         => 'kubernetes/nodes',
+        'priority'    => $priority++
+    ]
+);
+
+$section->add(
+    N_('Persistent Volume Claims'),
+    [
+        'description' => $this->translate('Persistent Volume Claims'),
+        'url'         => 'kubernetes/persistentvolumeclaims',
+        'priority'    => $priority++
+    ]
+);
+
+$section->add(
+    N_('Persistent Volumes'),
+    [
+        'description' => $this->translate('Persistent Volumes'),
+        'url'         => 'kubernetes/persistentvolumes',
+        'priority'    => $priority++
+    ]
+);
+
+$section->add(
+    N_('Pods'),
+    [
+        'description' => $this->translate('Pods'),
+        'url'         => 'kubernetes/pods',
+        'priority'    => $priority++
+    ]
+);
+
+$section->add(
+    N_('Replica Sets'),
+    [
+        'description' => $this->translate('Replica Sets'),
+        'url'         => 'kubernetes/replicasets',
+        'priority'    => $priority++
+    ]
+);
+
+$section->add(
+    N_('Secrets'),
+    [
+        'description' => $this->translate('Secrets'),
+        'url'         => 'kubernetes/secrets',
+        'priority'    => $priority++
+    ]
+);
+
+$section->add(
+    N_('Services'),
+    [
+        'description' => $this->translate('Services'),
+        'url'         => 'kubernetes/services',
+        'priority'    => $priority++
+    ]
+);
+
+$section->add(
+    N_('Stateful Sets'),
+    [
+        'description' => $this->translate('Stateful Sets'),
+        'url'         => 'kubernetes/statefulsets',
         'priority'    => $priority++
     ]
 );
