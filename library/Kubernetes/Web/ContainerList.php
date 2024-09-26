d5bb8fb8731981c2e55eaf0482e8854731133ae0
Eric Lippmann <eric.lippmann@icinga.com>
2024-09-26 13:22:52 +0200

    Disable `action-list` for containers
    Multiple action lists in a single view does not seem to work.

--- library/Kubernetes/Web/ContainerList.php
+++ library/Kubernetes/Web/ContainerList.php
@@ -8,6 +8,18 @@ use Icinga\Module\Kubernetes\Common\BaseItemList;
 
 class ContainerList extends BaseItemList
 {
+    /**
+     * Copied from {@see BaseItemList::$baseAttributes} but removed `action-list` as
+     * multiple action lists in a single view does not seem to work.
+     *
+     * @var array
+     */
+    protected array $baseAttributes = [
+        'class'                         => 'item-list',
+        'data-base-target'              => '_next',
+        'data-pdfexport-page-breaks-at' => '.list-item'
+    ];
+
     protected $defaultAttributes = ['class' => 'container-list'];
 
     protected function getItemClass(): string
