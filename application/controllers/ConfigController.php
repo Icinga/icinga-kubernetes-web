9abcb787040af1e9658ce84592a6c841ceaa0662
Eric Lippmann <eric.lippmann@icinga.com>
2024-11-18 20:44:38 +0100

    Refactor Notifications integration

--- application/controllers/ConfigController.php
+++ application/controllers/ConfigController.php
@@ -7,34 +7,24 @@ namespace Icinga\Module\Kubernetes\Controllers;
 use GuzzleHttp\Psr7\ServerRequest;
 use Icinga\Application\Config;
 use Icinga\Application\Logger;
+use Icinga\Exception\Http\HttpNotFoundException;
 use Icinga\Module\Kubernetes\Common\Database;
 use Icinga\Module\Kubernetes\Forms\DatabaseConfigForm;
 use Icinga\Module\Kubernetes\Forms\NotificationsConfigForm;
 use Icinga\Module\Kubernetes\Model\Config as KConfig;
 use Icinga\Module\Kubernetes\Web\Controller;
+use Icinga\Module\Notifications\Common\Database as NotificationsDatabase;
 use Icinga\Module\Notifications\Forms\SourceForm;
+use Icinga\Module\Notifications\Model\Source;
 use Icinga\Web\Notification;
 use Icinga\Web\Widget\Tabs;
 use ipl\Sql\Connection;
 use ipl\Stdlib\Filter;
+use LogicException;
 use Throwable;
 
 class ConfigController extends Controller
 {
-    protected const DEFAULT_SOURCE_USER = 'Icinga for Kubernetes';
-
-    protected const NOTIFICATIONS_SOURCE_ID = 'notifications.source_id';
-
-    protected const NOTIFICATIONS_URL = 'notifications.url';
-
-    protected const NOTIFICATIONS_KUBERNETES_WEB_URL = 'notifications.kubernetes_web_url';
-
-    protected const NOTIFICATIONS_USERNAME = 'notifications.username';
-
-    protected const NOTIFICATIONS_PASSWORD = 'notifications.password';
-
-    protected const NOTIFICATIONS_LOCKED = 'notifications.locked';
-
     protected bool $disableDefaultAutoRefresh = true;
 
     public function init()
@@ -46,84 +36,143 @@ class ConfigController extends Controller
 
     public function databaseAction(): void
     {
+        $this->mergeTabs($this->Module()->getConfigTabs()->activate('database'));
+
         $config = Config::module('kubernetes');
         $form = (new DatabaseConfigForm())
             ->populate($config->getSection('database'))
-            ->on(DatabaseConfigForm::ON_SUCCESS, function ($form) use ($config) {
+            ->on(DatabaseConfigForm::ON_SUCCESS, function (DatabaseConfigForm $form) use ($config) {
                 $config->setSection('database', $form->getValues());
                 $config->saveIni();
 
                 Notification::success($this->translate('New configuration has successfully been stored'));
             })->handleRequest($this->getServerRequest());
 
-        $this->mergeTabs($this->Module()->getConfigTabs()->activate('database'));
-
         $this->addContent($form);
     }
 
     public function notificationsAction()
     {
-        $db = Database::connection();
-        $dbConfig = KConfig::on($db);
-        $dbConfig->filter(
-            Filter::any(
-                Filter::equal('key', self::NOTIFICATIONS_URL),
-                Filter::equal('key', self::NOTIFICATIONS_KUBERNETES_WEB_URL),
-                Filter::equal('key', self::NOTIFICATIONS_USERNAME),
-                Filter::equal('key', self::NOTIFICATIONS_PASSWORD),
-                Filter::equal('key', self::NOTIFICATIONS_LOCKED)
-            )
-        );
+        $this->mergeTabs($this->Module()->getConfigTabs()->activate('notifications'));
 
-        $data = [];
+        $kconfig = [];
+        $q = KConfig::on(Database::connection())
+            ->filter(Filter::equal('key', [
+                KConfig::NOTIFICATIONS_URL,
+                KConfig::NOTIFICATIONS_USERNAME,
+                KConfig::NOTIFICATIONS_KUBERNETES_WEB_URL
+            ]));
 
-        foreach ($dbConfig as $pair) {
-            switch ($pair->key) {
-                case self::NOTIFICATIONS_URL:
-                    $data['notifications_url'] = $pair->value;
-                    break;
-                case self::NOTIFICATIONS_KUBERNETES_WEB_URL:
-                    $data['notifications_kubernetes_web_url'] = $pair->value;
-                    break;
-                case self::NOTIFICATIONS_USERNAME:
-                    $data['notifications_username'] = $pair->value;
-                    break;
-                case self::NOTIFICATIONS_PASSWORD:
-                    $data['notifications_password'] = $pair->value;
-                    break;
-            }
+        foreach ($q as $r) {
+            $kconfig[$r['key']] = $r;
         }
 
+        $sourceForm = new class (NotificationsDatabase::get()) extends SourceForm {
+            public function hasBeenSent(): bool
+            {
+                return true;
+            }
+
+            public function hasBeenSubmitted(): bool
+            {
+                return true;
+            }
+        };
+
         $form = (new NotificationsConfigForm())
-            ->populate($data)
-            ->on(NotificationsConfigForm::ON_SUCCESS,
-                function (NotificationsConfigForm $form) use ($db, $dbConfig) {
+            ->setKConfig($kconfig)
+            ->on(
+                NotificationsConfigForm::ON_SUCCESS,
+                function (NotificationsConfigForm $form) use ($kconfig, $sourceForm) {
                     if ($form->isLocked()) {
-                        Notification::error($this->translate('Notifications configuration is locked'));
+                        $form->addMessage($this->translate('Notifications configuration is locked.'));
+
                         return;
                     }
 
-                    $pressedButton = $form->getPressedSubmitElement();
-                    if ($pressedButton && $pressedButton->getName() === 'remove') {
-                        $this->deleteConfig($db);
+                    $values = $form->getValues();
 
-                        Notification::success($this->translate('Source has successfully been deleted'));
-                    } else {
-                        $this->generateSource(
-                            $form->getValue('notifications_url'),
-                            $form->getValue('notifications_kubernetes_web_url')
-                        );
+                    if (
+                        ! ($kconfig[KConfig::NOTIFICATIONS_USERNAME]->locked ?? false)
+                        && ($kconfig[KConfig::NOTIFICATIONS_USERNAME]->value ?? '') === ''
+                    ) {
+                        try {
+                            $values[KConfig::NOTIFICATIONS_PASSWORD] = $this->createSource($sourceForm);
+                        } catch (Throwable $e) {
+                            Logger::error($e);
+                            Logger::error($e->getTraceAsString());
 
-                        Notification::success(
-                            $this->translate('New configuration has successfully been stored')
-                        );
+                            $form->addMessage($e->getMessage());
+
+                            return;
+                        }
+
+                        /** @var ?Source $source */
+                        $source = Source::on(NotificationsDatabase::get())
+                            ->filter(Filter::all(
+                                Filter::equal('name', KConfig::DEFAULT_NOTIFICATIONS_NAME),
+                                Filter::equal('type', KConfig::DEFAULT_NOTIFICATIONS_TYPE)
+                            ))
+                            ->first();
+
+                        if ($source === null) {
+                            throw new LogicException($this->translate('Source not found'));
+                        }
+
+                        $values[KConfig::NOTIFICATIONS_USERNAME] = "source-$source->id";
                     }
+
+                    try {
+                        Database::connection()->transaction(function (Connection $db) use ($values) {
+                            $db->delete((new KConfig())->getTableName(), [
+                                sprintf('%s IN (?)', $db->quoteIdentifier('key')) => array_keys($values),
+                                'locked = ?'                                      => 'n'
+                            ]);
+
+                            foreach ($values as $k => $v) {
+                                if (empty($v)) {
+                                    continue;
+                                }
+
+                                $db->insert((new KConfig())->getTableName(), [
+                                    $db->quoteIdentifier('key') => $k,
+                                    'value'                     => $v,
+                                ]);
+                            }
+                        });
+                    } catch (Throwable $e) {
+                        Logger::error($e);
+                        Logger::error($e->getTraceAsString());
+
+                        $form->addMessage($e->getMessage());
+
+                        return;
+                    }
+
+                    Notification::success(
+                        $this->translate('New configuration has successfully been stored.')
+                    );
 
                     $this->redirectNow('__REFRESH__');
                 }
             )->handleRequest($this->getServerRequest());
 
-        $this->mergeTabs($this->Module()->getConfigTabs()->activate('notifications'));
+        if (
+            preg_match(
+                '/source-(\d+)/',
+                $kconfig[KConfig::NOTIFICATIONS_USERNAME]->value ?? '',
+                $matches
+            ) !== false
+            && ! empty($matches)
+        ) {
+            try {
+                $sourceForm->loadSource($matches[1]);
+
+                // TODO(el): Check password mismatch.
+            } catch (HttpNotFoundException $e) {
+                // TODO(el): Add error box.
+            }
+        }
 
         $this->addContent($form);
     }
@@ -140,149 +189,33 @@ class ConfigController extends Controller
         }
     }
 
-    protected function deleteConfig(Connection $db): void
+    protected function createSource(SourceForm $sourceForm): string
     {
-        $db->delete('config',
-            [
-                $db->quoteIdentifier('key') . ' = ?' => self::NOTIFICATIONS_SOURCE_ID
-            ]
-        );
-        $db->delete('config',
-            [
-                $db->quoteIdentifier('key') . ' = ?' => self::NOTIFICATIONS_URL
-            ]
-        );
-        $db->delete('config',
-            [
-                $db->quoteIdentifier('key') . ' = ?' => self::NOTIFICATIONS_KUBERNETES_WEB_URL
-            ]
-        );
-        $db->delete('config',
-            [
-                $db->quoteIdentifier('key') . ' = ?' => self::NOTIFICATIONS_USERNAME
-            ]
-        );
-        $db->delete('config',
-            [
-                $db->quoteIdentifier('key') . ' = ?' => self::NOTIFICATIONS_PASSWORD
-            ]
-        );
-    }
-
-    protected function generateSource(string $url, $kubernetes_web_url): void
-    {
-        $sourceForm = new class (\Icinga\Module\Notifications\Common\Database::get()) extends SourceForm {
-            public function hasBeenSubmitted()
-            {
-                return $this->hasBeenSent(); // Cheating :)
-            }
-        };
-
         $password = sha1(openssl_random_pseudo_bytes(16));
 
         $formData = [
             'listener_password'      => $password,
             'listener_password_dupe' => $password,
-            'name'                   => self::DEFAULT_SOURCE_USER,
-            'type'                   => 'kubernetes',
-            'icinga2_insecure_tls'   => 'n',
-        ];
-
-        $configData = [
-            'url'                => $url,
-            'kubernetes_web_url' => $kubernetes_web_url,
-            'password'           => $password,
+            'name'                   => KConfig::DEFAULT_NOTIFICATIONS_NAME,
+            'type'                   => KConfig::DEFAULT_NOTIFICATIONS_TYPE,
+            // TODO(el): Why?
+            'icinga2_insecure_tls'   => 'n'
         ];
 
         $sourceForm
             ->populate($formData)
-            ->on(SourceForm::ON_SUCCESS, function (SourceForm $form) use ($configData) {
-                try {
-                    $form->addSource();
-                } catch (Throwable $err) {
-                    Logger::error(
-                        'Failed to populate Icinga for Kubernetes notifications source: %s',
-                        $err
-                    );
-                    Logger::debug($err->getTraceAsString());
-
-                    return;
-                }
-
-                try {
-                    $this->generateConfigForSource($configData);
-                } catch (Throwable $err) {
-                    Logger::error('Failed to insert Icinga for Kubernetes notifications source ID: %s', $err);
-                    Logger::debug($err->getTraceAsString());
-                }
+            ->on(SourceForm::ON_SUCCESS, function (SourceForm $form) {
+                $form->addSource();
             });
 
-        // Again cheating to match the server request and our source form method.
-        $orgRequestMethod = $_SERVER['REQUEST_METHOD'];
-        $_SERVER['REQUEST_METHOD'] = 'POST';
-        $request = ServerRequest::fromGlobals();
-        $_SERVER['REQUEST_METHOD'] = $orgRequestMethod;
-
-        try {
-            $sourceForm->ensureAssembled();
-            $csrf = $sourceForm->getElement('CSRFToken');
-            if (preg_match('/ value="([^"]+)/', $csrf->getAttributes()->render(), $matches)) {
-                // CSRF token validation was changed in the meantime, so that it always triggers the validation,
-                // even without a populated token, so we need to workaround it here.
-                $csrf->setValue($matches[1]);
-            }
-
-            $sourceForm->handleRequest($request);
-        } catch (Throwable $err) {
-            $this->addError('kubernetes.source.error', time(), $err->getMessage());
-
-            Logger::error($err);
-            Logger::debug($err->getTraceAsString());
+        $sourceForm->ensureAssembled();
+        $csrf = $sourceForm->getElement('CSRFToken');
+        if (preg_match('/ value="([^"]+)/', $csrf->getAttributes()->render(), $matches)) {
+            $csrf->setValue($matches[1]);
         }
-    }
 
-    protected function generateConfigForSource(array $config): void
-    {
-        $sourceId = \Icinga\Module\Notifications\Common\Database::get()->lastInsertid();
-        $db = Database::connection();
-        $this->upsertConfig($db,
-            [
-                $db->quoteIdentifier('key') => self::NOTIFICATIONS_SOURCE_ID,
-                'value'                     => $sourceId,
-            ]);
-        $this->upsertConfig($db,
-            [
-                $db->quoteIdentifier('key') => self::NOTIFICATIONS_URL,
-                'value'                     => $config['url'],
-            ]);
-        $this->upsertConfig($db,
-            [
-                $db->quoteIdentifier('key') => self::NOTIFICATIONS_KUBERNETES_WEB_URL,
-                'value'                     => $config['kubernetes_web_url'],
-            ]);
-        $this->upsertConfig($db,
-            [
-                $db->quoteIdentifier('key') => self::NOTIFICATIONS_USERNAME,
-                'value'                     => self::DEFAULT_SOURCE_USER,
-            ]);
-        $this->upsertConfig($db,
-            [
-                $db->quoteIdentifier('key') => self::NOTIFICATIONS_PASSWORD,
-                'value'                     => $config['password'],
-            ]
-        );
-    }
+        $sourceForm->handleRequest(ServerRequest::fromGlobals());
 
-    protected function upsertConfig(Connection $db, array $data): void
-    {
-        $res = KConfig::on($db)->filter(Filter::equal('key', $data[$db->quoteIdentifier('key')]));
-        if (isset($res->first()->value)) {
-            $db->update('config',
-                ['value' => $data['value']],
-                [$db->quoteIdentifier('key') . ' = ?' => $data[$db->quoteIdentifier('key')]]
-            );
-        } else {
-            $db->insert('config', $data);
-        }
+        return $password;
     }
 }
--- application/forms/NotificationsConfigForm.php
+++ application/forms/NotificationsConfigForm.php
@@ -4,114 +4,154 @@
 
 namespace Icinga\Module\Kubernetes\Forms;
 
+use Icinga\Application\Icinga;
+use Icinga\Module\Kubernetes\Model\Config as KConfig;
 use ipl\Html\Attributes;
 use ipl\Html\Html;
+use ipl\Stdlib\Str;
+use ipl\Validator\CallbackValidator;
 use ipl\Web\Compat\CompatForm;
-use Icinga\Module\Kubernetes\Model\Config;
-use ipl\Stdlib\Filter;
-use Icinga\Module\Kubernetes\Common\Database;
+use ipl\Web\Url;
 
 class NotificationsConfigForm extends CompatForm
 {
-    protected function assemble(): void
+    protected array $kconfig = [];
+
+    public static function transformKeyForForm(string $key): string
     {
-        $submit = $this->createElement('submit', 'submit', [
-            'label'    => $this->translate('Save Changes'),
-            'disabled' => $this->isLocked() || $this->sourceAlreadyExists()
-        ]);
-        $this->registerElement($submit);
-        $this->decorate($submit);
-
-        $remove = $this->createElement('submit', 'remove', [
-            'label'          => $this->translate('Remove'),
-            'class'          => 'btn-remove',
-        ]);
-        $this->registerElement($remove);
-        $this->decorate($remove);
-
-        if ($this->isLocked()) {
-            $this->addHtml(
-                Html::tag('div', Attributes::create(['class' => 'control-group']), [
-                    Html::tag(
-                        'div',
-                        Attributes::create(['class' => 'control-label-group']),
-                    ),
-                    Html::tag(
-                        'p',
-                        Attributes::create(),
-                        "Notifications configuration is provided via YAML."
-                    )
-                ])
-            );
-        }
-
-        if ($this->sourceAlreadyExists()) {
-            $this->addHtml(
-                Html::tag('div', Attributes::create(['class' => 'control-group']), [
-                    Html::tag(
-                        'div',
-                        Attributes::create(['class' => 'control-label-group']),
-                    ),
-                    Html::tag(
-                        'p',
-                        Attributes::create(),
-                        "Notifications configuration has already a source."
-                    )
-                ])
-            );
-        }
-
-        $this->addElement('text', 'notifications_url',
-            [
-                'label'    => $this->translate('URL'),
-                'required' => true,
-                'disabled' => $this->isLocked() || $this->sourceAlreadyExists(),
-                'value'    => ''
-            ]
-        );
-
-        $this->addElement('text', 'notifications_kubernetes_web_url',
-            [
-                'label'    => $this->translate('Kubernetes Web URL'),
-                'required' => true,
-                'disabled' => $this->isLocked() || $this->sourceAlreadyExists(),
-                'value'    => ''
-            ]
-        );
-
-        if (! $this->sourceAlreadyExists()) {
-            $this->addHtml($submit);
-        } else {
-            $this->addHtml($remove);
-        }
+        return strtr($key, ['notifications.' => 'notifications_']);
     }
 
-    public function hasBeenSubmitted(): bool
+    public static function transformKeyForDb(string $key): string
     {
-        return $this->hasBeenSent() && ($this->getPopulatedValue('submit') || $this->getPopulatedValue('remove'));
+        return strtr($key, ['notifications_' => 'notifications.']);
+    }
+
+    public function getKConfig(): array
+    {
+        return $this->kconfig;
+    }
+
+    public function setKConfig(array $kconfig)
+    {
+        $this->kconfig = $kconfig;
+
+        return $this;
     }
 
     public function isLocked(): bool
     {
-        $config = Config::on(Database::connection());
-        $config->filter(Filter::equal('key', 'notifications.locked'));
+        $this->ensureAssembled();
 
-        if (isset($config->first()->value) && $config->first()->value === 'true') {
-            return true;
-        }
-
-        return false;
+        return array_reduce(
+            array_map(
+                fn($element) => $element->getAttributes()->get('disabled')->getValue(),
+                $this->getElements()
+            ),
+            fn(bool $carry, bool $item) => $carry && $item,
+            true
+        );
     }
 
-    public function sourceAlreadyExists()
+    public function getValue($name, $default = null)
     {
-        $config = Config::on(Database::connection());
-        $config->filter(Filter::equal('key', 'notifications.source_id'));
+        return parent::getValue(static::transformKeyForForm($name), $default);
+    }
 
-        if (isset($config->first()->value)) {
-            return true;
+    public function getValues(): array
+    {
+        $values = parent::getValues();
+
+        return array_combine(array_map([static::class, 'transformKeyForDb'], array_keys($values)), $values);
+    }
+
+    protected function getKubernetesWebUrl(): string
+    {
+        $r = Icinga::app()->getRequest();
+
+        list($host, $port) = Str::symmetricSplit($r->getServer('HTTP_HOST'), ':', 2);
+
+        $url = (Url::fromPath('kubernetes', [], $r))
+            ->setScheme($r->getScheme())
+            ->setHost($host)
+            ->setIsExternal();
+
+        if ($port !== null) {
+            $url->setPort($port);
         }
 
-        return false;
+        return $url->getAbsoluteUrl();
+    }
+
+    protected function isValidUri(string $uri): string
+    {
+        $components = parse_url($uri);
+
+        return $components !== false && isset($components['scheme'], $components['host']);
+    }
+
+//    public function hasBeenSubmitted(): bool
+//    {
+//        return $this->hasBeenSent() && ($this->getPopulatedValue('submit') || $this->getPopulatedValue('remove'));
+//    }
+
+    protected function assemble(): void
+    {
+        $this->addElement('text', static::transformKeyForForm(KConfig::NOTIFICATIONS_URL), [
+            'label'       => $this->translate('Icinga Notifications URL'),
+            'disabled'    => $this->kconfig[KConfig::NOTIFICATIONS_URL]->locked ?? false,
+            'value'       => $this->kconfig[KConfig::NOTIFICATIONS_URL]->value ?? null,
+            'placeholder' => 'http://localhost:5680',
+            'description' => $this->translate(
+                'Icinga Notifications URL. Leave/set blank to turn off notifications.'
+            ),
+            'validators'  => [
+                'Callback' => function ($value, CallbackValidator $validator) {
+                    if (! $this->isValidUri($value)) {
+                        $validator->addMessage($this->translate(
+                            'Invalid URL: Both host and scheme must be specified.'
+                        ));
+
+                        return false;
+                    }
+
+                    return true;
+                }
+            ]
+        ]);
+
+        $this->addElement('text', static::transformKeyForForm(KConfig::NOTIFICATIONS_KUBERNETES_WEB_URL), [
+            'label'      => $this->translate('Kubernetes Web URL'),
+            'required'   => true,
+            'disabled'   => $this->kconfig[KConfig::NOTIFICATIONS_KUBERNETES_WEB_URL]->locked ?? false,
+            'value'      => $this->kconfig[KConfig::NOTIFICATIONS_KUBERNETES_WEB_URL]->value ?? $this->getKubernetesWebUrl(),
+            'description' => $this->translate(
+                'Icinga Kubernetes Web URL.'
+            ),
+            'validators' => [
+                'Callback' => function ($value, CallbackValidator $validator) {
+                    if (! $this->isValidUri($value)) {
+                        $validator->addMessage($this->translate(
+                            'Invalid URL: Both host and scheme must be specified.'
+                        ));
+
+                        return false;
+                    }
+
+                    return true;
+                }
+            ]
+        ]);
+
+        $locked = $this->isLocked();
+
+        if ($locked) {
+            // TODO(el): Add locked info box.
+        }
+
+        $this->addElement('submit', 'submit', [
+            'label'    => $this->translate('Save Changes'),
+            'disabled' => $locked
+        ]);
     }
 }
