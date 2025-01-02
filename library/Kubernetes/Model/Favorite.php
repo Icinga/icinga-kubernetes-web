c44e849348741e6bf9a60b6e839d096a91208805
Johannes Rauh <johannes.rauh@icinga.com>
2025-01-02 13:22:22 +0100

    Add model for favorites

---
+++ library/Kubernetes/Model/Favorite.php
@@ -0,0 +1,54 @@
+<?php
+
+/* Icinga for Kubernetes Web | (c) 2023 Icinga GmbH | AGPLv3 */
+
+namespace Icinga\Module\Kubernetes\Model;
+
+use Icinga\Module\Kubernetes\Model\Behavior\Uuid;
+use ipl\I18n\Translation;
+use ipl\Orm\Behaviors;
+use ipl\Orm\Model;
+use ipl\Orm\Relations;
+
+class Favorite extends Model
+{
+    use Translation;
+
+    public function createBehaviors(Behaviors $behaviors): void
+    {
+        $behaviors->add(new Uuid([
+            'resource_uuid'
+        ]));
+    }
+
+    public function createRelations(Relations $relations)
+    {
+
+    }
+
+    public function getColumnDefinitions(): array
+    {
+        return [
+            'resource_uuid' => $this->translate('Resource UUID'),
+            'username'      => $this->translate('Username'),
+        ];
+    }
+
+    public function getColumns(): array
+    {
+        return [
+            'resource_uuid',
+            'username',
+        ];
+    }
+
+    public function getKeyName(): array
+    {
+        return ['resource_uuid', 'username'];
+    }
+
+    public function getTableName(): string
+    {
+        return 'favorite';
+    }
+}
