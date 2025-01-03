ab666ab77975dae01be51d1567014d7537f008d1
Johannes Rauh <johannes.rauh@icinga.com>
2025-01-03 14:02:43 +0100

    Return the matching ListItem classes

--- library/Kubernetes/Web/DaemonSetList.php
+++ library/Kubernetes/Web/DaemonSetList.php
@@ -5,13 +5,20 @@
 namespace Icinga\Module\Kubernetes\Web;
 
 use Icinga\Module\Kubernetes\Common\BaseItemList;
+use Icinga\Module\Kubernetes\Common\ViewMode;
 
 class DaemonSetList extends BaseItemList
 {
+    use ViewMode;
+
     protected $defaultAttributes = ['class' => 'daemon-set-list'];
 
     protected function getItemClass(): string
     {
-        return DaemonSetListItem::class;
+        return match ($this->getViewMode()) {
+            'minimal'  => DaemonSetListItemMinimal::class,
+            'detailed' => DaemonSetListItemDetailed::class,
+            default    => DaemonSetListItem::class,
+        };
     }
 }
