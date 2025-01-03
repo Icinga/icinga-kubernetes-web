bc063f63ade288bd4f991b074c7d60511cd98b00
Johannes Rauh <johannes.rauh@icinga.com>
2025-01-03 09:30:49 +0100

    Add ViewModeSwitcher

--- library/Kubernetes/Web/ListController.php
+++ library/Kubernetes/Web/ListController.php
@@ -4,14 +4,25 @@
 
 namespace Icinga\Module\Kubernetes\Web;
 
+use Exception;
+use GuzzleHttp\Psr7\ServerRequest;
+use Icinga\Application\Config;
+use Icinga\Application\Logger;
+use Icinga\Data\ConfigObject;
+use Icinga\Exception\Json\JsonDecodeException;
 use Icinga\Module\Kubernetes\Common\Auth;
 use Icinga\Module\Kubernetes\TBD\ObjectSuggestions;
+use Icinga\User\Preferences;
+use Icinga\User\Preferences\PreferencesStore;
+use Icinga\Util\Json;
 use Icinga\Web\Session;
 use ipl\Orm\Query;
 use ipl\Stdlib\Filter;
 use ipl\Web\Compat\SearchControls;
 use ipl\Web\Control\LimitControl;
+use ipl\Web\Control\PaginationControl;
 use ipl\Web\Control\SortControl;
+use ipl\Web\Url;
 use Ramsey\Uuid\Uuid;
 
 abstract class ListController extends Controller
@@ -52,9 +63,14 @@ abstract class ListController extends Controller
         $limitControl = $this->createLimitControl();
         $sortControl = $this->createSortControl($q, $this->getSortColumns());
         $paginationControl = $this->createPaginationControl($q);
+
+        $viewModeSwitcher = $this->createViewModeSwitcher($paginationControl, $limitControl);
+
         $searchBar = $this->createSearchBar($q, [
             $limitControl->getLimitParam(),
-            $sortControl->getSortParam()
+            $sortControl->getSortParam(),
+            $viewModeSwitcher->getViewModeParam(),
+            'columns'
         ]);
 
         if ($searchBar->hasBeenSent() && ! $searchBar->isValid()) {
@@ -75,10 +91,11 @@ abstract class ListController extends Controller
         $this->addControl($paginationControl);
         $this->addControl($sortControl);
         $this->addControl($limitControl);
+        $this->addControl($viewModeSwitcher);
         $this->addControl($searchBar);
 
         $contentClass = $this->getContentClass();
-        $this->addContent(new $contentClass($q));
+        $this->addContent((new $contentClass($q))->setViewMode($viewModeSwitcher->getViewMode()));
 
         if (! $searchBar->hasBeenSubmitted() && $searchBar->hasBeenSent()) {
             $this->sendMultipartUpdate();
@@ -93,6 +110,8 @@ abstract class ListController extends Controller
 
     abstract protected function getPermission(): string;
 
+    abstract protected function getIgnoredViewModes(): array;
+
     public function searchEditorAction(): void
     {
         $this->setTitle($this->translate('Adjust Filter'));
@@ -101,5 +120,159 @@ abstract class ListController extends Controller
             LimitControl::DEFAULT_LIMIT_PARAM,
             SortControl::DEFAULT_SORT_PARAM
         ]));
+    }
+
+    /**
+     * Create and return the ViewModeSwitcher
+     *
+     * This automatically shifts the view mode URL parameter from {@link $params}.
+     *
+     * @param PaginationControl $paginationControl
+     * @param LimitControl $limitControl
+     * @param bool $verticalPagination
+     *
+     * @return ViewModeSwitcher
+     */
+    public function createViewModeSwitcher(
+        PaginationControl $paginationControl,
+        LimitControl $limitControl,
+        bool $verticalPagination = false
+    ): ViewModeSwitcher {
+        $ignoredViewModes = $this->getIgnoredViewModes();
+
+        $viewModeSwitcher = new ViewModeSwitcher();
+        $viewModeSwitcher->setIdProtector([$this->getRequest(), 'protectId']);
+        $viewModeSwitcher->addIgnoredViewModes(...$ignoredViewModes);
+
+        $user = $this->Auth()->getUser();
+        if (($preferredModes = $user->getAdditional('kubernetes.view_modes')) === null) {
+            try {
+                $preferredModes = Json::decode(
+                    $user->getPreferences()->getValue('kubernetes', 'view_modes', '[]'),
+                    true
+                );
+            } catch (JsonDecodeException $e) {
+                Logger::error('Failed to load preferred view modes for user "%s": %s', $user->getUsername(), $e);
+                $preferredModes = [];
+            }
+
+            $user->setAdditional('kubernetes.view_modes', $preferredModes);
+        }
+
+        $requestRoute = $this->getRequest()->getUrl()->getPath();
+        if (isset($preferredModes[$requestRoute])) {
+            $viewModeSwitcher->setDefaultViewMode($preferredModes[$requestRoute]);
+        } else {
+            if (! in_array('detailed', $ignoredViewModes)) {
+                $viewModeSwitcher->setDefaultViewMode('detailed');
+            } else if (! in_array('common', $ignoredViewModes)) {
+                $viewModeSwitcher->setDefaultViewMode('common');
+            } else {
+                $viewModeSwitcher->setDefaultViewMode('minimal');
+            }
+        }
+
+        $viewModeSwitcher->populate([
+            $viewModeSwitcher->getViewModeParam() => $this->params->shift($viewModeSwitcher->getViewModeParam())
+        ]);
+
+        $session = $this->Window()->getSessionNamespace('kubernetes-viewmode-' . $this->Window()->getContainerId());
+
+        $viewModeSwitcher->on(
+            ViewModeSwitcher::ON_SUCCESS,
+            function (ViewModeSwitcher $viewModeSwitcher) use (
+                $user,
+                $preferredModes,
+                $paginationControl,
+                $verticalPagination,
+                &$session
+            ) {
+                $viewMode = $viewModeSwitcher->getValue($viewModeSwitcher->getViewModeParam());
+                $requestUrl = Url::fromRequest();
+
+                $preferredModes[$requestUrl->getPath()] = $viewMode;
+                $user->setAdditional('kubernetes.view_modes', $preferredModes);
+
+                try {
+                    $preferencesStore = PreferencesStore::create(new ConfigObject([
+                        //TODO: Don't set store key as it will no longer be needed once we drop support for
+                        // lower version of icingaweb2 then v2.11.
+                        //https://github.com/Icinga/icingaweb2/pull/4765
+                        'store'    => Config::app()->get('global', 'config_backend', 'db'),
+                        'resource' => Config::app()->get('global', 'config_resource')
+                    ]), $user);
+                    $preferencesStore->load();
+                    $preferencesStore->save(
+                        new Preferences(['kubernetes' => ['view_modes' => Json::encode($preferredModes)]])
+                    );
+                } catch (Exception $e) {
+                    Logger::error('Failed to save preferred view mode for user "%s": %s', $user->getUsername(), $e);
+                }
+
+                $pageParam = $paginationControl->getPageParam();
+                $limitParam = LimitControl::DEFAULT_LIMIT_PARAM;
+                $currentPage = $paginationControl->getCurrentPageNumber();
+
+                $requestUrl->setParam($viewModeSwitcher->getViewModeParam(), $viewMode);
+                if (! $requestUrl->hasParam($limitParam)) {
+                    if ($viewMode === 'minimal') {
+                        $session->set('previous_page', $currentPage);
+                        $session->set('request_path', $requestUrl->getPath());
+
+                        $limit = $paginationControl->getLimit();
+                        if (! $verticalPagination) {
+                            // We are computing it based on the first element being rendered on this current page
+                            $currentPage = (int) (floor((($currentPage * $limit) - $limit) / ($limit * 2)) + 1);
+                        } else {
+                            $currentPage = (int) (round($currentPage * $limit / ($limit * 2)));
+                        }
+
+                        $session->set('current_page', $currentPage);
+                    } elseif (
+                        $viewModeSwitcher->getDefaultViewMode() === 'minimal'
+                    ) {
+                        $limit = $paginationControl->getLimit();
+                        if ($currentPage === $session->get('current_page')) {
+                            // No other page numbers have been selected, i.e the user only
+                            // switches back and forth without changing the page numbers
+                            $currentPage = $session->get('previous_page');
+                        } elseif (! $verticalPagination) {
+                            $currentPage = (int) (floor((($currentPage * $limit) - $limit) / ($limit / 2)) + 1);
+                        } else {
+                            $currentPage = (int) (floor($currentPage * $limit / ($limit / 2)));
+                        }
+
+                        $session->clear();
+                    }
+
+                    if (($requestUrl->hasParam($pageParam) && $currentPage > 1) || $currentPage > 1) {
+                        $requestUrl->setParam($pageParam, $currentPage);
+                    } else {
+                        $requestUrl->remove($pageParam);
+                    }
+                }
+
+                $this->redirectNow($requestUrl);
+            }
+        )->handleRequest(ServerRequest::fromGlobals());
+
+        $viewMode = $viewModeSwitcher->getViewMode();
+        if ($viewMode === 'minimal' || $viewMode === 'common') {
+            $hasLimitParam = Url::fromRequest()->hasParam($limitControl->getLimitParam());
+
+            if ($paginationControl->getDefaultPageSize() <= LimitControl::DEFAULT_LIMIT && ! $hasLimitParam) {
+                $paginationControl->setDefaultPageSize($paginationControl->getDefaultPageSize() * 2);
+                $limitControl->setDefaultLimit($limitControl->getDefaultLimit() * 2);
+
+                $paginationControl->apply();
+            }
+        }
+
+        $requestPath = $session->get('request_path');
+        if ($requestPath && $requestPath !== $requestRoute) {
+            $session->clear();
+        }
+
+        return $viewModeSwitcher;
     }
 }
