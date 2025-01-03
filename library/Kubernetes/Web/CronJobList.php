854ef3529d1c06742cb2de2602553509572d18f8
Johannes Rauh <johannes.rauh@icinga.com>
2025-01-03 14:02:43 +0100

    Return the matching ListItem classes

--- library/Kubernetes/Web/CronJobList.php
+++ library/Kubernetes/Web/CronJobList.php
@@ -5,13 +5,20 @@
 namespace Icinga\Module\Kubernetes\Web;
 
 use Icinga\Module\Kubernetes\Common\BaseItemList;
+use Icinga\Module\Kubernetes\Common\ViewMode;
 
 class CronJobList extends BaseItemList
 {
+    use ViewMode;
+
     protected $defaultAttributes = ['class' => 'cronjob-list'];
 
     protected function getItemClass(): string
     {
-        return CronJobListItem::class;
+        return match ($this->getViewMode()) {
+            'minimal'  => CronJobListItemMinimal::class,
+            'detailed' => CronJobListItemDetailed::class,
+            default    => CronJobListItem::class,
+        };
     }
 }
