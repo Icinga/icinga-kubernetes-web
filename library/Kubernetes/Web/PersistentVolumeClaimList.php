980b373d3e93292ef02750cb7b923afb03a8edc9
Johannes Rauh <johannes.rauh@icinga.com>
2025-01-14 12:26:59 +0100

    Switch between view modes in one file

--- library/Kubernetes/Web/PersistentVolumeClaimList.php
+++ library/Kubernetes/Web/PersistentVolumeClaimList.php
@@ -5,20 +5,13 @@
 namespace Icinga\Module\Kubernetes\Web;
 
 use Icinga\Module\Kubernetes\Common\BaseItemList;
-use Icinga\Module\Kubernetes\Common\ViewMode;
 
 class PersistentVolumeClaimList extends BaseItemList
 {
-    use ViewMode;
-
     protected $defaultAttributes = ['class' => 'pvc-list'];
 
     protected function getItemClass(): string
     {
-        return match ($this->getViewMode()) {
-            'minimal'  => PersistentVolumeClaimListItemMinimal::class,
-            'detailed' => PersistentVolumeClaimListItemDetailed::class,
-            default    => PersistentVolumeClaimListItem::class,
-        };
+        return PersistentVolumeClaimListItem::class;
     }
 }
--- library/Kubernetes/Web/PersistentVolumeList.php
+++ library/Kubernetes/Web/PersistentVolumeList.php
@@ -5,20 +5,13 @@
 namespace Icinga\Module\Kubernetes\Web;
 
 use Icinga\Module\Kubernetes\Common\BaseItemList;
-use Icinga\Module\Kubernetes\Common\ViewMode;
 
 class PersistentVolumeList extends BaseItemList
 {
-    use ViewMode;
-
     protected $defaultAttributes = ['class' => 'persistent-volume-list'];
 
     protected function getItemClass(): string
     {
-        return match ($this->getViewMode()) {
-            'minimal'  => PersistentVolumeListItemMinimal::class,
-            'detailed' => PersistentVolumeListItemDetailed::class,
-            default    => PersistentVolumeListItem::class,
-        };
+        return PersistentVolumeListItem::class;
     }
 }
