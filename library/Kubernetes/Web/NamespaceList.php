393b14180a7825cc474c48e7520804df060fa61c
Johannes Rauh <johannes.rauh@icinga.com>
2025-01-14 12:26:59 +0100

    Switch between view modes in one file

--- library/Kubernetes/Web/NamespaceList.php
+++ library/Kubernetes/Web/NamespaceList.php
@@ -5,20 +5,13 @@
 namespace Icinga\Module\Kubernetes\Web;
 
 use Icinga\Module\Kubernetes\Common\BaseItemList;
-use Icinga\Module\Kubernetes\Common\ViewMode;
 
 class NamespaceList extends BaseItemList
 {
-    use ViewMode;
-
     protected $defaultAttributes = ['class' => 'namespace-list'];
 
     protected function getItemClass(): string
     {
-        return match ($this->getViewMode()) {
-            'minimal'  => NamespaceListItemMinimal::class,
-            'detailed' => NamespaceListItemDetailed::class,
-            default    => NamespaceListItem::class,
-        };
+        return NamespaceListItem::class;
     }
 }
