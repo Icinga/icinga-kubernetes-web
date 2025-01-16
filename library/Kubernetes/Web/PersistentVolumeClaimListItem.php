e4d309b5b50f0a83c2091bc0911a77415d7a434d
Johannes Rauh <johannes.rauh@icinga.com>
2025-01-16 09:11:45 +0100

    Use ->viewMode instead of ->getViewMode()

--- library/Kubernetes/Web/PersistentVolumeClaimListItem.php
+++ library/Kubernetes/Web/PersistentVolumeClaimListItem.php
@@ -39,7 +39,7 @@ class PersistentVolumeClaimListItem extends BaseListItem
 
     protected function assembleHeader(BaseHtmlElement $header): void
     {
-        match ($this->getViewMode()) {
+        match ($this->viewMode) {
             ViewModeSwitcher::VIEW_MODE_MINIMAL,
             ViewModeSwitcher::VIEW_MODE_COMMON   =>
             $header->addHtml(
@@ -69,11 +69,11 @@ class PersistentVolumeClaimListItem extends BaseListItem
     {
         $main->addHtml($this->createHeader());
 
-        if ($this->getViewMode() === ViewModeSwitcher::VIEW_MODE_DETAILED) {
+        if ($this->viewMode === ViewModeSwitcher::VIEW_MODE_DETAILED) {
             $main->addHtml($this->createCaption());
         }
 
-        if ($this->getViewMode() !== ViewModeSwitcher::VIEW_MODE_MINIMAL) {
+        if ($this->viewMode !== ViewModeSwitcher::VIEW_MODE_MINIMAL) {
             $main->addHtml($this->createFooter());
         }
     }
