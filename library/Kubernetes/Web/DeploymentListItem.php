0c066e31338cc05eba4a36b24fb5f5e6702b8ea7
Johannes Rauh <johannes.rauh@icinga.com>
2025-01-22 15:34:37 +0100

    Use default traits for item list parts

--- library/Kubernetes/Web/DeploymentListItem.php
+++ library/Kubernetes/Web/DeploymentListItem.php
@@ -5,65 +5,27 @@
 namespace Icinga\Module\Kubernetes\Web;
 
 use Icinga\Module\Kubernetes\Common\BaseListItem;
+use Icinga\Module\Kubernetes\Common\DefaultListItemCaption;
+use Icinga\Module\Kubernetes\Common\DefaultListItemHeader;
+use Icinga\Module\Kubernetes\Common\DefaultListItemMain;
+use Icinga\Module\Kubernetes\Common\DefaultListItemTitle;
+use Icinga\Module\Kubernetes\Common\DefaultListItemVisual;
 use Icinga\Module\Kubernetes\Common\Format;
-use Icinga\Module\Kubernetes\Common\Links;
 use ipl\Html\Attributes;
 use ipl\Html\BaseHtmlElement;
-use ipl\Html\Html;
-use ipl\Html\HtmlDocument;
 use ipl\Html\HtmlElement;
-use ipl\Html\Text;
 use ipl\I18n\Translation;
 use ipl\Web\Widget\HorizontalKeyValue;
 use ipl\Web\Widget\Icon;
-use ipl\Web\Widget\Link;
-use ipl\Web\Widget\StateBall;
-use ipl\Web\Widget\TimeAgo;
 
 class DeploymentListItem extends BaseListItem
 {
     use Translation;
-
-    protected function assembleHeader(BaseHtmlElement $header): void
-    {
-        match ($this->viewMode) {
-            ViewModeSwitcher::VIEW_MODE_MINIMAL,
-            ViewModeSwitcher::VIEW_MODE_COMMON   =>
-            $header->addHtml(
-                Html::tag(
-                    'span',
-                    Attributes::create(['class' => 'header-minimal']),
-                    [
-                        $this->createTitle(),
-                        $this->createCaption()
-                    ]
-                )
-            ),
-            ViewModeSwitcher::VIEW_MODE_DETAILED =>
-            $header->addHtml($this->createTitle()),
-            default                              => null
-        };
-
-        $header->addHtml(new TimeAgo($this->item->created->getTimestamp()));
-    }
-
-    protected function assembleCaption(BaseHtmlElement $caption): void
-    {
-        $caption->addHtml(new Text($this->item->icinga_state_reason));
-    }
-
-    protected function assembleMain(BaseHtmlElement $main): void
-    {
-        $main->addHtml($this->createHeader());
-
-        if ($this->viewMode === ViewModeSwitcher::VIEW_MODE_DETAILED) {
-            $main->addHtml($this->createCaption());
-        }
-
-        if ($this->viewMode !== ViewModeSwitcher::VIEW_MODE_MINIMAL) {
-            $main->addHtml($this->createFooter());
-        }
-    }
+    use DefaultListItemHeader;
+    use DefaultListItemCaption;
+    use DefaultListItemMain;
+    use DefaultListItemTitle;
+    use DefaultListItemVisual;
 
     protected function assembleFooter(BaseHtmlElement $footer): void
     {
@@ -102,38 +64,5 @@ class DeploymentListItem extends BaseListItem
                 Format::seconds($this->item->progress_deadline_seconds)
             )
         );
-    }
-
-    protected function assembleTitle(BaseHtmlElement $title): void
-    {
-        $title->addHtml(Html::sprintf(
-            $this->translate('%s is %s', '<deployment> is <icinga_state>'),
-            [
-                new HtmlElement(
-                    'span',
-                    new Attributes(['class' => 'namespace-badge']),
-                    new HtmlElement('i', new Attributes(['class' => 'icon kicon-namespace'])),
-                    new Text($this->item->namespace)
-                ),
-                new Link(
-                    (new HtmlDocument())->addHtml(
-                        new HtmlElement('i', new Attributes(['class' => 'icon kicon-deployment'])),
-                        new Text($this->item->name)
-                    ),
-                    Links::deployment($this->item),
-                    new Attributes(['class' => 'subject'])
-                )
-            ],
-            new HtmlElement(
-                'span',
-                new Attributes(['class' => 'icinga-state-text']),
-                new Text($this->item->icinga_state)
-            )
-        ));
-    }
-
-    protected function assembleVisual(BaseHtmlElement $visual): void
-    {
-        $visual->addHtml(new StateBall($this->item->icinga_state, StateBall::SIZE_MEDIUM));
     }
 }
--- library/Kubernetes/Web/PodListItem.php
+++ library/Kubernetes/Web/PodListItem.php
@@ -5,73 +5,33 @@
 namespace Icinga\Module\Kubernetes\Web;
 
 use Icinga\Module\Kubernetes\Common\BaseListItem;
-use Icinga\Module\Kubernetes\Common\Links;
+use Icinga\Module\Kubernetes\Common\DefaultListItemCaption;
+use Icinga\Module\Kubernetes\Common\DefaultListItemHeader;
+use Icinga\Module\Kubernetes\Common\DefaultListItemMain;
+use Icinga\Module\Kubernetes\Common\DefaultListItemTitle;
+use Icinga\Module\Kubernetes\Common\DefaultListItemVisual;
 use Icinga\Module\Kubernetes\Model\Container;
 use Icinga\Module\Kubernetes\Model\InitContainer;
 use Icinga\Module\Kubernetes\Model\SidecarContainer;
-use ipl\Html\Attributes;
 use ipl\Html\BaseHtmlElement;
-use ipl\Html\Html;
-use ipl\Html\HtmlDocument;
-use ipl\Html\HtmlElement;
-use ipl\Html\Text;
 use ipl\I18n\Translation;
 use ipl\Web\Widget\HorizontalKeyValue;
 use ipl\Web\Widget\Icon;
-use ipl\Web\Widget\Link;
-use ipl\Web\Widget\StateBall;
-use ipl\Web\Widget\TimeAgo;
 
 class PodListItem extends BaseListItem
 {
     use Translation;
+    use DefaultListItemHeader;
+    use DefaultListItemCaption;
+    use DefaultListItemMain;
+    use DefaultListItemTitle;
+    use DefaultListItemVisual;
 
     public const QOS_ICONS = [
         'BestEffort' => 'eraser',
         'Burstable'  => 'life-ring',
         'Guaranteed' => 'heart-pulse'
     ];
-
-    protected function assembleHeader(BaseHtmlElement $header): void
-    {
-        match ($this->viewMode) {
-            ViewModeSwitcher::VIEW_MODE_MINIMAL,
-            ViewModeSwitcher::VIEW_MODE_COMMON   =>
-            $header->addHtml(
-                Html::tag(
-                    'span',
-                    Attributes::create(['class' => 'header-minimal']),
-                    [
-                        $this->createTitle(),
-                        $this->createCaption()
-                    ]
-                )
-            ),
-            ViewModeSwitcher::VIEW_MODE_DETAILED =>
-            $header->addHtml($this->createTitle()),
-            default                              => null
-        };
-
-        $header->addHtml(new TimeAgo($this->item->created->getTimestamp()));
-    }
-
-    protected function assembleCaption(BaseHtmlElement $caption): void
-    {
-        $caption->addHtml(new Text($this->item->icinga_state_reason));
-    }
-
-    protected function assembleMain(BaseHtmlElement $main): void
-    {
-        $main->addHtml($this->createHeader());
-
-        if ($this->viewMode === ViewModeSwitcher::VIEW_MODE_DETAILED) {
-            $main->addHtml($this->createCaption());
-        }
-
-        if ($this->viewMode !== ViewModeSwitcher::VIEW_MODE_MINIMAL) {
-            $main->addHtml($this->createFooter());
-        }
-    }
 
     protected function assembleFooter(BaseHtmlElement $footer): void
     {
@@ -132,38 +92,5 @@ class PodListItem extends BaseListItem
                 ->addAttributes(['class' => 'push-left']),
             new HorizontalKeyValue(new Icon('share-nodes'), $this->item->node_name ?? $this->translate('None'))
         );
-    }
-
-    protected function assembleTitle(BaseHtmlElement $title): void
-    {
-        $title->addHtml(Html::sprintf(
-            $this->translate('%s is %s', '<pod> is <icinga_state>'),
-            [
-                new HtmlElement(
-                    'span',
-                    new Attributes(['class' => 'namespace-badge']),
-                    new HtmlElement('i', new Attributes(['class' => 'icon kicon-namespace'])),
-                    new Text($this->item->namespace)
-                ),
-                new Link(
-                    (new HtmlDocument())->addHtml(
-                        new HtmlElement('i', new Attributes(['class' => 'icon kicon-pod'])),
-                        new Text($this->item->name)
-                    ),
-                    Links::pod($this->item),
-                    new Attributes(['class' => 'subject'])
-                )
-            ],
-            new HtmlElement(
-                'span',
-                new Attributes(['class' => 'icinga-state-text']),
-                new Text($this->item->icinga_state)
-            )
-        ));
-    }
-
-    protected function assembleVisual(BaseHtmlElement $visual): void
-    {
-        $visual->addHtml(new StateBall($this->item->icinga_state, StateBall::SIZE_MEDIUM));
     }
 }
