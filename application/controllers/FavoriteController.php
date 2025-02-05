d63c6885f874705952ab2e7d024db678e6dd7d54
Johannes Rauh <johannes.rauh@icinga.com>
2025-02-05 11:45:38 +0100

    Insert and delete with priority column
    When inserting a new favorite, the new favorite gets the priority + 1 of
    the favorite with the currently highest priority. If no favorite is
    stored for the kind the new favorite gets the priority 0.
    
    When deleting a favorite, all favorites with higher priority are
    decremented by 1.

--- application/controllers/FavoriteController.php
+++ application/controllers/FavoriteController.php
@@ -7,8 +7,11 @@ namespace Icinga\Module\Kubernetes\Controllers;
 use Icinga\Application\Logger;
 use Icinga\Module\Kubernetes\Common\Auth;
 use Icinga\Module\Kubernetes\Common\Database;
+use Icinga\Module\Kubernetes\Model\Favorite;
 use Icinga\Module\Kubernetes\Web\Controller;
 use Icinga\Module\Kubernetes\Web\FavoriteToggleForm;
+use ipl\Sql\Expression;
+use ipl\Stdlib\Filter;
 use Ramsey\Uuid\Uuid;
 use Throwable;
 
@@ -25,27 +28,70 @@ class FavoriteController extends Controller
     {
         (new FavoriteToggleForm(true))
             ->on(FavoriteToggleForm::ON_SUCCESS, function (FavoriteToggleForm $form) {
-                $uuid = $this->params->get('uuid');
+                $db = Database::connection();
+                $uuid = Uuid::fromString($this->params->get('uuid'))->getBytes();
                 $kind = $this->params->get('kind');
+                $username = Auth::getInstance()->getUser()->getUsername();
                 $checked = $form->getValue('favorite-checkbox');
+
                 try {
                     if ($checked) {
-                        Database::connection()->insert(
+                        $highestPriorityFavorite = Favorite::on($db)
+                            ->columns('priority')
+                            ->filter(Filter::all(
+                                Filter::equal('kind', $kind),
+                            ))
+                            ->orderBy('priority', SORT_DESC)
+                            ->first();
+
+                        $db->insert(
                             'favorite',
                             [
-                                'resource_uuid' => Uuid::fromString($uuid)->getBytes(),
+                                'resource_uuid' => $uuid,
                                 'kind'          => $kind,
-                                'username'      => Auth::getInstance()->getUser()->getUsername(),
+                                'username'      => $username,
+                                'priority'      => ($highestPriorityFavorite?->priority ?? -1) + 1,
                             ]
                         );
                     } else {
-                        Database::connection()->delete(
+                        $transactionStarted = false;
+                        if (! $db->inTransaction()) {
+                            $transactionStarted = true;
+                            $db->beginTransaction();
+                        }
+
+                        $favoriteToDelete = Favorite::on($db)
+                            ->filter(Filter::all(
+                                Filter::equal('resource_uuid', $uuid),
+                                Filter::equal('username', $username)
+                            ))
+                            ->first();
+
+                        $db->delete(
                             'favorite',
                             [
-                                'resource_uuid = ?' => Uuid::fromString($uuid)->getBytes(),
-                                'username = ?'      => Auth::getInstance()->getUser()->getUsername(),
+                                'resource_uuid = ?' => $uuid,
+                                'username = ?'      => $username,
                             ]
                         );
+
+                        $affectedFavorites = Favorite::on($db)
+                            ->columns(['resource_uuid', 'username'])
+                            ->filter(Filter::equal('kind', $favoriteToDelete->kind))
+                            ->filter(Filter::greaterThan('priority', $favoriteToDelete->priority))
+                            ->orderBy('priority', SORT_ASC);
+
+                        foreach ($affectedFavorites as $favorite) {
+                            $db->update(
+                                'favorite',
+                                ['priority' => new Expression('priority - 1')],
+                                ['resource_uuid = ?' => $favorite->resource_uuid, 'username = ?' => $favorite->username]
+                            );
+                        }
+
+                        if ($transactionStarted) {
+                            $db->commitTransaction();
+                        }
                     }
                 } catch (Throwable $e) {
                     Logger::error($e);
