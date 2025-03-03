b97225ab23628b887e2a0ecb11e986dead6523b5
Johannes Rauh <johannes.rauh@icinga.com>
2025-03-03 12:00:38 +0100

    Dashboard to show all favorites

--- library/Kubernetes/Web/Factory.php
+++ library/Kubernetes/Web/Factory.php
@@ -41,7 +41,7 @@ abstract class Factory
             return 'persistentvolumeclaim';
         }
 
-        return strtolower(str_replace(['_', '-'], '', $kind));
+        return strtolower(str_replace([' ', '_', '-'], '', $kind));
     }
 
     public static function createIcon(string $kind): ?ValidHtml
@@ -282,6 +282,29 @@ abstract class Factory
         }
 
         return null;
+    }
+
+    public static function createTitle(string $kind): string
+    {
+        return match ($kind) {
+            'configmap'             => 'Config Maps',
+            'cronjob'               => 'Cron Jobs',
+            'daemonset'             => 'Daemon Sets',
+            'deployment'            => 'Deployments',
+            'event'                 => 'Events',
+            'ingress'               => 'Ingresses',
+            'job'                   => 'Jobs',
+            'namespace'             => 'Namespaces',
+            'node'                  => 'Nodes',
+            'persistentvolume'      => 'Persistent Volumes',
+            'persistentvolumeclaim' => 'Persistent Volume Claims',
+            'pod'                   => 'Pods',
+            'replicaset'            => 'Replica Sets',
+            'secret'                => 'Secrets',
+            'service'               => 'Services',
+            'statefulset'           => 'Stateful Sets',
+            default                 => null,
+        };
     }
 
     public static function getKindFromModel(Model $model): string
