cd2ea49aec7d6db63167f38a3035f06c2f9b312c
Johannes Rauh <johannes.rauh@icinga.com>
2025-05-13 07:10:55 +0200

    Full width for list items in detail view

--- library/Kubernetes/Web/JobDetail.php
+++ library/Kubernetes/Web/JobDetail.php
@@ -29,7 +29,7 @@ class JobDetail extends BaseHtmlElement
 
     protected $tag = 'div';
 
-    protected $defaultAttributes = ['class' => 'job-detail'];
+    protected $defaultAttributes = ['class' => 'object-detail job-detail'];
 
     public function __construct(Job $job)
     {
