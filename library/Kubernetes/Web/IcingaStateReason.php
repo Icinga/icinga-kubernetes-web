14c7bfa5a026154f6551154638f96418a8cd133b
Johannes Rauh <johannes.rauh@icinga.com>
2025-05-21 13:23:43 +0200

    Add widget to represent icinga state reason row

--- library/Kubernetes/Web/IcingaStateReason.php
+++ library/Kubernetes/Web/IcingaStateReason.php
@@ -5,38 +5,9 @@
 namespace Icinga\Module\Kubernetes\Web;
 
 use ipl\Html\BaseHtmlElement;
-use ipl\Html\HtmlString;
-use ipl\Web\Widget\Icon;
-use ipl\Web\Widget\StateBall;
 
 class IcingaStateReason extends BaseHtmlElement
 {
-    /** @var string[] Patterns to be replaced in plain text plugin output */
-    public const TEXT_PATTERNS = [
-        '~\\\n~',
-        '~([\[(])OK([])])?~',
-        '~([\[(])WARNING([])])?~',
-        '~([\[(])CRITICAL([])])?~',
-        '~([\[(])UNKNOWN([])])?~',
-        '~([\[(])UP([])])?~',
-        '~([\[(])DOWN([])])?~',
-        '~([\[(])PENDING([])])?~',
-        '~\\\\_~',
-    ];
-
-    /** @var string[] Replacements for {@see static::TEXT_PATTERNS} */
-    public const TEXT_REPLACEMENTS = [
-        "\n",
-        '<span class="state-ball ball-size-m state-ok"></span>',
-        '<span class="state-ball ball-size-m state-warning"></span>',
-        '<span class="state-ball ball-size-m state-critical"></span>',
-        '<span class="state-ball ball-size-m state-unknown"></span>',
-        '<span class="state-ball ball-size-m state-up"></span>',
-        '<span class="state-ball ball-size-m state-down"></span>',
-        '<span class="state-ball ball-size-m state-pending"></span>',
-        '<i class="icon fa-arrow-turn-up fa level-indicator"></i>',
-    ];
-
     protected $tag = 'div';
 
     protected $defaultAttributes = ['class' => 'icinga-state-reason'];
@@ -49,33 +20,21 @@ class IcingaStateReason extends BaseHtmlElement
 
     protected function assemble(): void
     {
-        [$kind, $message] = explode(' ', $this->icingaStateReason, 2);
+        [$kind, $namespaceSlashName, $message] = explode(' ', $this->icingaStateReason, 3);
 
-        $content = preg_replace(
-            self::TEXT_PATTERNS,
-            self::TEXT_REPLACEMENTS,
-            htmlspecialchars(
-                trim($message),
-                ENT_COMPAT | ENT_SUBSTITUTE | ENT_HTML5,
-                null,
-                false
-            )
-        );
+        $name = (str_contains($namespaceSlashName, '/') ? explode('/', $namespaceSlashName)[1] : $namespaceSlashName);
 
-        // Add zero-width space after commas which are not followed by a whitespace character
-        // in oder to help browsers to break words in plugin output.
-        $content = preg_replace('/,(?=\S)/', ',&#8203;', $content);
+        $this->addHtml(new IcingaStateReasonRow(
+            $this->icingaState,
+            $kind,
+            $name,
+            $message,
+            $this->buildTooltip($kind, $namespaceSlashName)
+        ));
+    }
 
-        if ($this->icingaState) {
-            $this->addHtml(new StateBall($this->icingaState, StateBall::SIZE_MEDIUM));
-        }
-
-        if (strtolower($kind) === 'node') {
-            $this->addHtml(new Icon('share-nodes'));
-        } else {
-            $this->addHtml(new KIcon(strtolower($kind)));
-        }
-
-        $this->addHtml(new HtmlString($content));
+    protected function buildTooltip(string $kind, string $name): string
+    {
+        return sprintf('%s %s', $kind, $name);
     }
 }
---
+++ library/Kubernetes/Web/IcingaStateReasonRow.php
@@ -0,0 +1,45 @@
+<?php
+
+/* Icinga for Kubernetes Web | (c) 2025 Icinga GmbH | AGPLv3 */
+
+namespace Icinga\Module\Kubernetes\Web;
+
+use ipl\Html\Attributes;
+use ipl\Html\BaseHtmlElement;
+use ipl\Html\HtmlElement;
+use ipl\Html\Text;
+use ipl\Web\Widget\Icon;
+use ipl\Web\Widget\StateBall;
+
+class IcingaStateReasonRow extends BaseHtmlElement
+{
+    protected $tag = 'div';
+
+    protected $defaultAttributes = ['class' => 'row'];
+
+    public function __construct(
+        protected string $state,
+        protected string $kind,
+        protected string $name,
+        protected string $reason,
+        protected string $tooltip,
+        protected ?string $parentName = null
+    ) {
+    }
+
+    public function assemble(): void
+    {
+        $kind = strtolower($this->kind);
+        $this->addHtml(
+            new StateBall(strtolower($this->state), StateBall::SIZE_MEDIUM),
+            ($kind === 'node' ? new Icon('share-nodes') : new KIcon($kind)),
+            new HtmlElement(
+                'span',
+                new Attributes(['class' => 'text']),
+                (new HighlightDelta($this->name, $this->parentName ?? ''))
+                    ->addAttributes(['class' => 'tooltip-holder', 'title' => $this->tooltip]),
+                new HtmlElement('span', new Attributes(['class' => 'reason']), new Text($this->reason))
+            )
+        );
+    }
+}
