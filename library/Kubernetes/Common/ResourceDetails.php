7d05c4af339d13b5a1f3dff6c6ab55ce81faeb4b
Johannes Rauh <johannes.rauh@icinga.com>
2025-05-21 13:12:59 +0200

    Use namespace badge in detail view

--- library/Kubernetes/Common/ResourceDetails.php
+++ library/Kubernetes/Common/ResourceDetails.php
@@ -7,6 +7,10 @@ namespace Icinga\Module\Kubernetes\Common;
 use AppendIterator;
 use ArrayIterator;
 use Generator;
+use Icinga\Module\Kubernetes\Web\KIcon;
+use ipl\Html\Attributes;
+use ipl\Html\HtmlElement;
+use ipl\Html\Text;
 use ipl\I18n\Translation;
 use IteratorAggregate;
 use Traversable;
@@ -30,7 +34,12 @@ class ResourceDetails implements IteratorAggregate
         $iterator = new AppendIterator();
         $iterator->append(new ArrayIterator([
             $this->translate('Name')             => $this->resource->name,
-            $this->translate('Namespace')        => $this->resource->namespace,
+            $this->translate('Namespace')        => new HtmlElement(
+                'span',
+                new Attributes(['class' => 'namespace-badge']),
+                new KIcon('namespace'),
+                new Text($this->resource->namespace)
+            ),
             $this->translate('UID')              => $this->resource->uid,
             $this->translate('Resource Version') => $this->resource->resource_version,
             $this->translate('Created')          => $this->resource->created->format('Y-m-d H:i:s')
